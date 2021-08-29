903a6e05e87f5150a866ef306742a30debee7b85
Hunter2807 <46440327+Hunter2807@users.noreply.github.com>
2021-08-30 00:07:30 +0530

    Update together.py

--- discord/together.py
+++ discord/together.py
@@ -1,4 +1,9 @@
 class ActivityLink:
+    """A meta class which takes in the payload sent by the API, as the only argument, and generates an
+    invite link from that.
+    
+    BEWARE- THIS CLASS IS NOT INTENTED TO BE USED IN GENERAL AND IS JUST MEANT FOR DOCUMENTATION PURPOSES"""
+
     def __init__(self, payload: dict):
         self.code = payload.pop(code)
     def __repr__(self):
