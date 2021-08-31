3fb4bca32ffc8a12c8adfdc13ce55186882beea1
m1raynee <71838742+m1raynee@users.noreply.github.com>
2021-08-31 16:27:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,12 +54,12 @@ packages = [
     'discord.ext.tasks',
 ]
 
-setup(name='discord.py',
-      author='Rapptz',
-      url='https://github.com/Rapptz/discord.py',
+setup(name='disnake',
+      author='EQUENOS & Rapptz',
+      url='https://github.com/EQUENOS/disnake',
       project_urls={
-        "Documentation": "https://discordpy.readthedocs.io/en/latest/",
-        "Issue tracker": "https://github.com/Rapptz/discord.py/issues",
+        "Documentation": "https://discordpy.readthedocs.io/en/latest/",  # TODO: docs link
+        "Issue tracker": "https://github.com/EQUENOS/disnake/issues",
       },
       version=version,
       packages=packages,
