3fc8f53cce1b6a779af0fbeb79a245cb46bc1828
Abhigyan Tripathi <abhigyantrips@gmail.com>
2021-09-18 17:02:06 +0530

    Created new example - user_echo_webhook.py
    This is a script wherein the user can input a UserID as well as message content, and thebot will create a webhook to "impersonate" specified user, so to speak.

---
+++ examples/user_echo_webhook.py
@@ -0,0 +1,45 @@
+import disnake
+from disnake.ext import commands
+
+description = """This is a script wherein the user can input 
+                 a UserID as well as message content, and the
+                 bot will create a webhook to "impersonate" 
+                 specified user, so to speak."""
+
+intents = disnake.Intents.all() # Not limiting the permissions, since this is an example - set this accordingly.
+
+client = commands.Bot(command_prefix = "!", description = description, intents = intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print('------')
+
+@client.command()
+@commands.has_permissions(administrator=True) # To make sure that not everyone can use this command.
+async def userecho(ctx, member: disnake.Member, *, content):
+  
+    await ctx.message.delete() # We don't want users to see who initiated the command, to make it more realistic :P
+    
+    current_webhooks = await ctx.message.channel.webhooks() # This is the webhook via which our message will be sent.
+    new_webhook = ''
+    webhook_count = []
+    
+    # A set of loops for checks on existing webhooks, and whether creation/editing/deletion is required.
+    for webhook in current_webhooks:
+        if webhook.name == "Bot Webhook":
+            webhook_count.append(webhook)
+    if (len(webhook_count) > 1):
+        new_webhook = await ctx.message.channel.create_webhook(name='Butternaan Webhook', reason="Bot Webhook")
+        for webhook in webhook_count:
+            await webhook.delete()
+    elif len(webhook_count) == 0:
+        new_webhook = await ctx.message.channel.create_webhook(name='Butternaan Webhook', reason="Bot Webhook")
+    elif len(webhook_count) == 1:
+        for webhook in webhook_count:
+            new_webhook = webhook
+    # Finally, sending the message via the webhook - the bot will fetch the user's display name and avatar.
+    # NOTE: This method cannot impersonate the member's roles, since it works using webhooks.
+    await new_webhook.send(content=content, username=member.display_name, avatar_url=member.avatar)
+    
+client.run("YOUR_TOKEN_HERE")
