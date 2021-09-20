9e8659e2d36f384c3236da545a24557e7946f38a
sadru <thesadru@gmail.com>
2021-09-20 21:53:11 +0200

    Implement the basics of Param

---
+++ disnake/ext/commands/params.py
@@ -0,0 +1,209 @@
+"""Repsonsible for handling Params for slash commands"""
+from __future__ import annotations
+
+import inspect
+from enum import Enum, EnumMeta
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Literal, Type, TypeVar, Union, get_origin, get_type_hints
+
+import disnake
+from disnake.app_commands import Option, OptionChoice
+from disnake.enums import OptionType, try_enum_to_int
+
+if TYPE_CHECKING:
+    from disnake.interactions import ApplicationCommandInteraction as Interaction
+
+TChoice = TypeVar("TChoice", str, int)
+
+__all__ = (
+    "extract_params",
+    "param",
+    "option_enum",
+)
+
+
+class Param:
+    """
+    Parameters
+    ----------
+    default: Union[:class:`str`, Callable[[:class:`ApplicationCommandInteraction`, Any], Any]]
+        default value or a default value factory
+    name: :class:`str`
+        option's name, the parameter name by default
+    description: :class:`str`
+        option's description
+    converter: Callable[[:class:`ApplicationCommandInteraction`, Any], Any]
+        the option's converter, takes in an interaction and the argument
+    choices: List[:class:`OptionChoice`]
+    type: :class:`type`
+    """
+
+    TYPES: Dict[type, int] = {
+        str: 3,
+        int: 4,
+        bool: 5,
+        disnake.abc.User: 6,
+        disnake.User: 6,
+        disnake.Member: 6,
+        disnake.abc.GuildChannel: 7,
+        disnake.TextChannel: 7,
+        disnake.VoiceChannel: 7,
+        disnake.CategoryChannel: 7,
+        disnake.StageChannel: 7,
+        disnake.StoreChannel: 7,
+        disnake.Role: 8,
+        Union[disnake.Member, disnake.Role]: 9,
+        disnake.abc.Snowflake: 9,
+        float: 10,
+    }
+
+    def __init__(
+        self,
+        default: Any = ...,
+        *,
+        name: str = '',
+        description: str = None,
+        converter: Callable[[Interaction, Any], Any] = None,
+        choices: List[OptionChoice] = None,
+        type: type = str,
+    ) -> None:
+        self.default = default
+        self.name = name
+        self.param_name = name
+        self.description = description or "\u200b"
+        self.converter = converter
+
+        self.choices = choices or []
+        self.type = type
+
+    @property
+    def required(self):
+        return self.default is ...
+
+    @property
+    def discord_type(self) -> OptionType:
+        return OptionType(self.TYPES.get(self.type, OptionType.string.value))
+
+    @discord_type.setter
+    def discord_type(self, discord_type: OptionType) -> None:
+        value = try_enum_to_int(discord_type)
+        for t, v in self.TYPES.items():
+            if value == v:
+                self.type = t
+                return
+
+        raise TypeError(f"Type {discord_type} is not a valid Param type")
+
+    def __repr__(self):
+        return f"<Param default={self.default!r} name={self.name!r} description={self.description!r}>"
+
+    def get_default(self, inter: Interaction) -> Any:
+        """Gets the default for an interaction"""
+        if callable(self.default):
+            return self.default(inter)
+        
+        return self.default
+
+    def parse_annotation(self, annotation: Any) -> None:
+        if annotation is inspect.Parameter.empty or annotation is Any:
+            pass
+        elif isinstance(annotation, EnumMeta):
+            self.choices = [OptionChoice(name, value) for name, value in annotation.__members__.items()]  # type: ignore
+            self.type = type(self.choices[0].value)
+        elif get_origin(annotation) is Literal:
+            self.choices = [OptionChoice(str(i), i) for i in annotation.__args__]
+            self.type = type(self.choices[0].value)
+        elif annotation in self.TYPES:
+            self.type = annotation
+        else:
+            raise TypeError(f"{annotation!r} is not a valid Param annotation")
+
+    def parse_parameter(self, param: inspect.Parameter) -> None:
+        self.name = self.name or param.name.replace("_", "-")
+        self.param_name = param.name
+
+    def to_option(self) -> Option:
+        if self.name == '':
+            raise TypeError("Param must be parsed first")
+
+        return Option(
+            name=self.name,
+            description=self.description,
+            type=self.discord_type,
+            required=self.required,
+            choices=self.choices,
+        )
+
+
+def extract_params(func: Callable, cog: Any = None) -> List[Param]:
+    """Extract params from a function signature"""
+    sig = inspect.signature(func)
+    parameters = list(sig.parameters.values())
+    if cog is None:
+        # hacky I suppose
+        cog = parameters[0].name == 'self'
+    parameters = parameters[2:] if cog else parameters[1:]
+    type_hints = get_type_hints(func)
+
+    params = []
+    for parameter in parameters:
+        if parameter.kind in [inspect.Parameter.VAR_POSITIONAL, inspect.Parameter.VAR_KEYWORD]:
+            continue
+        param = parameter.default
+        if not isinstance(param, Param):
+            param = Param(param if param is not parameter.empty else ...)
+
+        param.parse_parameter(parameter)
+        param.parse_annotation(type_hints.get(parameter.name, Any))
+        params.append(param)
+
+    return params
+
+
+def create_connector(params: List[Param]) -> Dict[str, Any]:
+    """Create a connector for each param"""
+    return {
+        param.name: param.param_name
+        for param in params
+        if param.name != param.param_name
+    }
+
+
+def resolve_param_kwargs(func: Callable, inter: Interaction, kwargs: Dict[str, Any]) -> Dict[str, Any]:
+    """Resolves a call with kwargs and transforms into normal kwargs
+    
+    Depends on the fact that optionparams already contain all info.
+    """
+    sig = inspect.signature(func)
+
+    for parameter in sig.parameters.values():
+        if parameter.kind in [inspect.Parameter.VAR_POSITIONAL, inspect.Parameter.VAR_KEYWORD]:
+            continue
+        param = parameter.default
+        if not isinstance(param, Param):
+            continue
+
+        kwargs.setdefault(param.param_name, param.get_default(inter))
+        # TODO: Converters
+
+    return kwargs
+
+def param(
+    default: Any = ...,
+    *,
+    name: str = '',
+    desc: str = None,
+    description: str = None,
+    conv: Callable[[Interaction, Any], Any] = None,
+    converter: Callable[[Interaction, Any], Any] = None,
+    choices: List[OptionChoice] = None,
+) -> Any:
+    return Param(default, name=name, description=desc or description, converter=conv or converter, choices=choices)
+
+
+def option_enum(choices: Union[Dict[str, TChoice], List[TChoice]], **kwargs: TChoice) -> Type[TChoice]:
+    if isinstance(choices, list):
+        choices = {str(i): i for i in choices}
+
+    choices = choices or kwargs
+    first, *_ = choices.values()
+    return Enum("", choices, type=type(first))
--- disnake/ext/commands/slash_core.py
+++ disnake/ext/commands/slash_core.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, List, Tuple, Union, TYPE_CHECKING, Callable
 
 from .base_core import InvokableApplicationCommand, _get_overridden_method
 from .errors import *
+from .params import extract_params, create_connector, resolve_param_kwargs
 
 from disnake.app_commands import SlashCommand, Option
 from disnake.enums import OptionType
@@ -40,7 +41,7 @@ class SubCommandGroup(InvokableApplicationCommand):
             type=OptionType.sub_command_group,
             options=[]
         )
-        self.qualified_name: str = None
+        self.qualified_name: str = ''
 
     def sub_command(
         self,
@@ -90,14 +91,24 @@ class SubCommand(InvokableApplicationCommand):
         **kwargs
     ):
         super().__init__(func, name=name, **kwargs)
-        self.connectors: Dict[str, str] = connectors
+        self.connectors: Dict[str, str] = connectors or {}
+        
+        if not options:
+            params = extract_params(func, self.cog)
+            options = [param.to_option() for param in params]
+            self.connectors.update(create_connector(params))
+        
         self.option = Option(
             name=self.name,
             description=description or '-',
             type=OptionType.sub_command,
             options=options
         )
-        self.qualified_name = None
+        self.qualified_name = ''
+    
+    async def invoke(self, inter: ApplicationCommandInteraction, *args, **kwargs) -> None:
+        kwargs = resolve_param_kwargs(self.callback, inter, kwargs)
+        return await super().invoke(inter, *args, **kwargs)
 
 
 class InvokableSlashCommand(InvokableApplicationCommand):
@@ -115,10 +126,16 @@ class InvokableSlashCommand(InvokableApplicationCommand):
         **kwargs
     ):
         super().__init__(func, name=name, **kwargs)
-        self.connectors: Dict[str, str] = connectors
+        self.connectors: Dict[str, str] = connectors or {}
         self.children: Dict[str, Union[SubCommand, SubCommandGroup]] = {}
         self.auto_sync: bool = auto_sync
-        self.guild_ids: List[int] = guild_ids
+        self.guild_ids: List[int] = guild_ids or []
+
+        if not options:
+            params = extract_params(func, self.cog)
+            options = [param.to_option() for param in params]
+            self.connectors.update(create_connector(params))
+        
         self.body = SlashCommand(
             name=self.name,
             description=description or '-',
@@ -211,7 +228,7 @@ class InvokableSlashCommand(InvokableApplicationCommand):
                 if local is not None:
                     await local(inter, error)
         finally:
-            inter.bot.dispatch('slash_command_error', inter, error)
+            inter.bot.dispatch('slash_command_error', inter, error) # type: ignore
 
     async def invoke_children(self, inter: ApplicationCommandInteraction):
         chain, kwargs = options_as_route(inter.options)
@@ -224,19 +241,22 @@ class InvokableSlashCommand(InvokableApplicationCommand):
             subcmd = self.children.get(chain[0])
         elif len(chain) == 2:
             group = self.children.get(chain[0])
+            assert isinstance(group, SubCommandGroup)
             subcmd = group.children.get(chain[1]) if group is not None else None
+        else:
+            raise ValueError("Chain too long")
 
         if group is not None:
             try:
                 await group.invoke(inter)
-            except Exception as exc:
+            except CommandError as exc:
                 await group._call_local_error_handler(inter, exc)
                 raise
         
         if subcmd is not None:
             try:
                 await subcmd.invoke(inter, **kwargs)
-            except Exception as exc:
+            except CommandError as exc:
                 await subcmd._call_local_error_handler(inter, exc)
                 raise
 
@@ -248,7 +268,8 @@ class InvokableSlashCommand(InvokableApplicationCommand):
                 await self(inter)
                 await self.invoke_children(inter)
             else:
-                await self(inter, **inter.options)
+                kwargs = resolve_param_kwargs(self.callback, inter, inter.options or {})
+                await self(inter, **kwargs)
         except CommandError:
             inter.command_failed = True
             raise
@@ -260,7 +281,7 @@ class InvokableSlashCommand(InvokableApplicationCommand):
             raise CommandInvokeError(exc) from exc
         finally:
             if self._max_concurrency is not None:
-                await self._max_concurrency.release(inter)
+                await self._max_concurrency.release(inter) # type: ignore
 
             await self.call_after_hooks(inter)
 
