91e1839f1697799adc205791b2739733cbf78616
sadru <thesadru@gmail.com>
2021-09-24 16:42:31 +0200

    Implement channel_types
    Also rewrote the slash command example

---
+++ examples/slash_commands/old.py
@@ -0,0 +1,21 @@
+"""
+An example of old-style options.
+"""
+import disnake
+from disnake.ext import commands
+
+bot = commands.Bot("!")
+
+@bot.slash_command(
+    name="Slash Command",
+    description="A Simple Slash Command",
+    options=[
+        disnake.Option("string", description="A string to send", required=True),
+        disnake.Option("channel", description="The destination channel", type=disnake.OptionType.channel),
+        disnake.Option("number", description="The number of repetitions", type=disnake.OptionType.integer),
+    ]
+)
+async def command(inter, string, channel = None, number = 1):
+    channel = channel or inter.channel
+    await inter.response.send_message(f"Sending {string} {number}x to {channel.mention}", ephemeral=True)
+    await channel.send(string * number)
