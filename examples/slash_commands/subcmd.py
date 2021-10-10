71c7c9fb1149b2db0ec682eff8b2c7923ab032d2
LEv145 <40071054+LEv145@users.noreply.github.com>
2021-10-10 17:31:56 +0200

    Mypy struct, flake8 fix

--- examples/slash_commands/subcmd.py
+++ examples/slash_commands/subcmd.py
@@ -1,10 +1,11 @@
-import disnake
+from disnake import ApplicationCommandInteraction
 from disnake.ext import commands
+
 
 bot = commands.Bot("!")
 
 # Slash command subcommands differ from classic text subcommands & groups
-# 
+#
 # The gist of it is:
 #   You may define any slash command which takes no arguments as the subcommand parent.
 #   A parent has children which are subcommands or subcommand groups
@@ -14,36 +15,38 @@ bot = commands.Bot("!")
 
 # Define a new command with two children:
 @bot.slash_command()
-async def command(inter):
+async def command(_inter: ApplicationCommandInteraction) -> None:
     print("This code is ran every time any subcommand is invoked")
 
-@command.sub_command()
-async def foo(inter, option: str):
-    await inter.response.send_message(f"Received {option}")
 
 @command.sub_command()
-async def bar(inter, option: int):
+async def foo(inter: ApplicationCommandInteraction, option: str) -> None:
+    await inter.response.send_message(f"Received {option}")
+
+
+@command.sub_command()
+async def bar(inter: ApplicationCommandInteraction, option: int) -> None:
     await inter.response.send_message(f"Gotten {option}")
 
 
 # Define a new command with sub command groups (this time in a cog)
 class MyCog(commands.Cog):
     @bot.slash_command()
-    async def command(self, inter):
+    async def command(self, _inter: ApplicationCommandInteraction) -> None:
         print("This code is ran every time any subcommand is invoked")
 
     @command.sub_command_group()
-    async def foo(self, inter):
+    async def foo(self, _inter: ApplicationCommandInteraction) -> None:
         print("This code is ran every time any subcommand of foo is invoked")
 
     @foo.sub_command()
-    async def a(self, inter, option: int):
+    async def a(self, inter: ApplicationCommandInteraction, option: int) -> None:
         await inter.response.send_message(f"You ran /command foo a {option}")
 
     @command.sub_command_group()
-    async def bar(self, inter):
+    async def bar(self, _inter: ApplicationCommandInteraction) -> None:
         print("This code is ran every time any subcommand of bar is invoked")
-    
+
     @bar.sub_command()
-    async def b(self, inter, option: float):
+    async def b(self, inter: ApplicationCommandInteraction, option: float) -> None:
         await inter.response.send_message(f"You ran /command bar b {option}")
