a8a7650143c2e7360e993aa308b671903d60162f
LEv145 <40071054+LEv145@users.noreply.github.com>
2021-10-10 17:43:18 +0200

    Fix for mypy strict and flake 8

--- examples/edits.py
+++ examples/edits.py
@@ -1,20 +1,25 @@
-import disnake
 import asyncio
+
+import disnake
+
 
 class MyClient(disnake.Client):
     async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
+        print("------")
 
-    async def on_message(self, message):
-        if message.content.startswith('!editme'):
-            msg = await message.channel.send('10')
+    async def on_message(self, message: disnake.Message) -> None:
+        if message.content.startswith("!editme"):
+            channel_message = await message.channel.send("10")
             await asyncio.sleep(3.0)
-            await msg.edit(content='40')
+            await channel_message.edit(content="40")
 
-    async def on_message_edit(self, before, after):
-        msg = f'**{before.author}** edited their message:\n{before.content} -> {after.content}'
-        await before.channel.send(msg)
+    async def on_message_edit(self, before: disnake.Message, after: disnake.Message) -> None:
+        await before.channel.send(
+            f"**{before.author}** edited their message:\n"
+            f"{before.content} -> {after.content}"
+        )
+
 
 client = MyClient()
 client.run('token')
