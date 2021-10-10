e5422d325a27fe97fe600326c83299e34ad2d12a
LEv145 <40071054+LEv145@users.noreply.github.com>
2021-10-10 17:38:30 +0200

    Fix for mypy strict and flake 8

--- examples/slash_commands/old.py
+++ examples/slash_commands/old.py
@@ -2,10 +2,15 @@
 An example of old-style options.
 Not the most convenient syntax.
 """
+from typing import Optional, Union
+
 import disnake
+from disnake import TextChannel, Thread, ApplicationCommandInteraction
 from disnake.ext import commands
 
+
 bot = commands.Bot("!")
+
 
 @bot.slash_command(
     name="slash_command",
@@ -16,7 +21,12 @@ bot = commands.Bot("!")
         disnake.Option("number", description="The number of repetitions", type=disnake.OptionType.integer),
     ]
 )
-async def command(inter, string, channel = None, number = 1):
-    channel = channel or inter.channel
+async def command(
+    inter: ApplicationCommandInteraction,
+    string: str,
+    channel: Optional[Union[TextChannel, Thread]] = None,
+    number: int = 1
+) -> None:
+    channel = channel if channel is not None else inter.channel
     await inter.response.send_message(f"Sending {string} {number}x to {channel.mention}", ephemeral=True)
     await channel.send(string * number)
