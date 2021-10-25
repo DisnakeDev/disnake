30e93a798d41b1982041a262db938f31c40e9064
sadru <thesadru@gmail.com>
2021-10-25 20:31:38 +0200

    Fix weird comment formatting

--- examples/views/disable_view.py
+++ examples/views/disable_view.py
@@ -3,42 +3,40 @@ from disnake.ext import commands
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"))
 
-# Define a simple View with buttons
+
 class MyView(disnake.ui.View):
-
+    
     def __init__(self):
-
-    # Sets the timeout
         super().__init__(timeout=30.0)
 
-    # The timeout, after which the first button will be disabled and the second button will be removed
     async def on_timeout(self):
-        self.children[0].disabled = True    # First button is disabled
-        self.remove_item(self.children[1])  # Second button is removed
-        await self.message.edit(view=self)  # View is edited
+    	# Once the view times out we disable the first button and remove the second button
+        self.children[0].disabled = True
+        self.remove_item(self.children[1])
+        # make sure to update the message with the new buttons
+        await self.message.edit(view=self)
 
-    # Button that will disable the view
     @disnake.ui.button(label="Click to disable the view", style=disnake.ButtonStyle.red)
     async def disable(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
 
-	# Disabling the view
+        # We disable every single component in this view
         for child in self.children:
-            child.disabled = True           # Disabling all the components in the view
-        await self.message.edit(view=self)  # Editing the view
+            child.disabled = True
+        # make sure to update the message with the new buttons
+        await self.message.edit(view=self)
 
-        # Prevents `on_timeout` from being triggered after the buttons are disabled
+        # Prevents on_timeout from being triggered after the buttons are disabled
         self.stop()
 
     @disnake.ui.button(label="Click to remove the view", style=disnake.ButtonStyle.red)
     async def remove(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
-
         # view = None removes the view
         await self.message.edit(view=None)
 
 @bot.command()
 async def view(ctx):
 
-    # Defines our view so that we can use `self.message` in `on_timeout` to edit it
+    # Defines our view so that we can use the message in on_timeout to edit it
     view = MyView()
 
     # Sends a message with the view
