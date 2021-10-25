a7bb252d8eac477db452fab1970d7f8695c92c93
shiftinv <me@shiftinv.cc>
2021-10-26 00:23:37 +0200

    Add student hub channel type + features

--- disnake/guild.py
+++ disnake/guild.py
@@ -207,6 +207,8 @@ class Guild(Hashable):
         - ``COMMUNITY``: Guild is a community server.
         - ``DISCOVERABLE``: Guild shows up in Server Discovery.
         - ``FEATURABLE``: Guild is able to be featured in Server Discovery.
+        - ``HAS_DIRECTORY_ENTRY``: Guild is listed in a student hub.
+        - ``HUB``: Guild is a student hub.
         - ``INVITE_SPLASH``: Guild's invite page can have a special splash.
         - ``MEMBER_VERIFICATION_GATE_ENABLED``: Guild has Membership Screening enabled.
         - ``MONETIZATION_ENABLED``: Guild has enabled monetization.
--- disnake/invite.py
+++ disnake/invite.py
@@ -47,6 +47,7 @@ if TYPE_CHECKING:
     from .types.channel import (
         PartialChannel as InviteChannelPayload,
     )
+    from .types.guild import GuildFeature
     from .state import ConnectionState
     from .guild import Guild
     from .abc import GuildChannel
@@ -160,7 +161,7 @@ class PartialInviteGuild:
         self._state: ConnectionState = state
         self.id: int = id
         self.name: str = data['name']
-        self.features: List[str] = data.get('features', [])
+        self.features: List[GuildFeature] = data.get('features', [])
         self._icon: Optional[str] = data.get('icon')
         self._banner: Optional[str] = data.get('banner')
         self._splash: Optional[str] = data.get('splash')
