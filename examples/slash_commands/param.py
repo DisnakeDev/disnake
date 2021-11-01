6a10dc61d776eab7dbcd1f50d710a90a4daa0ce0
Victor <67214928+Victorsitou@users.noreply.github.com>
2021-11-01 14:29:25 -0300

    Oops

--- examples/slash_commands/param.py
+++ examples/slash_commands/param.py
@@ -177,7 +177,7 @@ async def languages(inter: disnake.ApplicationCommandInteraction, language: str)
 @languages.autocomplete("language")
 async def language_autocomp(
     inter: disnake.ApplicationCommandInteraction, string: str
-) -> List[str]
+) -> List[str]:
     langs = ["Python", "JavaScript", "TypeScript", "Java", "Rust", "Lisp", "Elixir"]
     string = string.lower()
     return [lang for lang in langs if string in lang.lower()] 
