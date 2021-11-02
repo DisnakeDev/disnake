ee3bcc8c9821a8c744423b84f4e5e4fa195b0440
Victorsitou <67214928+Victorsitou@users.noreply.github.com>
2021-11-02 17:26:26 -0300

    Add min/max value example.

---
+++ examples/slash_commands/min_max_value.py
@@ -0,0 +1,70 @@
+import disnake
+from disnake.ext import commands
+import asyncio
+import secrets
+
+bot = commands.Bot(command_prefix="-")
+
+# Here we create a simple sequence of messages for the following command.
+
+SEQUENCE = [
+    """‎‎‎‎‎‎‎‏‏‎  ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‏‏‎‏🚀
+Rocket launch starting in 0.""",
+    """‏‏‎   ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏🚀
+\nRocket launch starting in 0.""",
+    """‏‏‎ ‏‏‎ ‎‏‏‎ ‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‏‏‎🚀
+\n
+Rocket launch starting in 0.
+""",
+]
+
+
+# For this example to how to use min and max value in slash commands,
+# we are going to do a rocket launch.
+
+
+@bot.slash_command(name="rocket-launch", description="Launch a rocket!")
+async def rocket_launch(
+    interaction: disnake.ApplicationCommandInteraction,
+    seconds: int = commands.Param(min_value=5, max_value=15, description="Seconds to countdown."),
+):
+
+    # In this case, we will use min/max value for the seconds to count before the launch.
+
+    await interaction.response.send_message(f"Rocket launch starting in {seconds}.")
+    await asyncio.sleep(1)
+
+    # Here we will do a countdown with the seconds input that the user gave.
+    for i in range(1, seconds + 1):
+
+        await interaction.edit_original_message(content=f"Rocket launch starting in {seconds - i}.")
+        await asyncio.sleep(1)
+
+    # Here we send the sequence of messages for the rocket launch,
+    # we will edit the message in order to send the sequence.
+    for sequencee in SEQUENCE:
+        await interaction.edit_original_message(content=sequencee)
+        await asyncio.sleep(1)
+
+    # Here we will choice between "success" and "explosion", in order to know if the rocket launch was success or not.
+    rocket = secrets.choice(["success", "explosion"])
+
+    if rocket == "success":
+        # If the rocket launch was a success, we will edit the message and send that it was success.
+        success = [
+            """‏‏‎‏‏‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‏‏‎🚀
+    \n\nRocket launched successfully!"""
+        ]
+
+        await interaction.edit_original_message(content=success[0])
+    else:
+        # If the rocket launch wasn't a succes and it exploded, we will edit the message and send that the rocket exploded.
+        explosion = [
+            """‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎ ‏‏‎ ‎‏‏‎ ‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‏‏‎💥
+    \n\nOh no, the rocket exploded."""
+        ]
+
+        await interaction.edit_original_message(content=explosion[0])
+
+
+bot.run("OTAzNDIwMjI5NDMzMDQ0OTky.YXstpw.F5AFU1wM8guiOoZV1h-6n4ZdZ40")
