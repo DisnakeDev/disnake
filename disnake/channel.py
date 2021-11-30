b0325e3fd411c355f66667af8351758f587c4687
Andrew Zhu <me@andrew-zhu.com>
2021-11-29 16:21:49 -0800

    docs: fix linter complaint

--- disnake/channel.py
+++ disnake/channel.py
@@ -1042,7 +1042,7 @@ class VoiceChannel(disnake.abc.Messageable, VocalGuildChannel):
 
     def is_nsfw(self) -> bool:
         """Checks if the channel is NSFW.
-        
+
         :return type: :class:`bool`
         """
         return self.nsfw
