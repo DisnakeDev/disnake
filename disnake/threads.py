0c8ae635cc1ae928f756f6b89c0b0f9ebf9840a4
Victorsitou <67214928+Victorsitou@users.noreply.github.com>
2021-12-05 02:19:51 -0300

    `ArchiveDuration` -> `ThreadArchiveDurationT`

--- disnake/threads.py
+++ disnake/threads.py
@@ -30,7 +30,7 @@ import asyncio
 
 from .mixins import Hashable
 from .abc import Messageable
-from .enums import ChannelType, try_enum, ThreadArchiveDuration as ArchiveDuration
+from .enums import ChannelType, try_enum, ThreadArchiveDuration as ThreadArchiveDurationT
 from .errors import ClientException
 from .utils import MISSING, parse_time, snowflake_time, _get_as_snowflake
 
@@ -588,7 +588,7 @@ class Thread(Messageable, Hashable):
         if archived is not MISSING:
             payload["archived"] = archived
         if auto_archive_duration is not MISSING:
-            if isinstance(auto_archive_duration, ArchiveDuration):
+            if isinstance(auto_archive_duration, ThreadArchiveDurationT):
                 payload["auto_archive_duration"] = auto_archive_duration.value
             else:
                 payload["auto_archive_duration"] = auto_archive_duration
