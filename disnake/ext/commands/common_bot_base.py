717c73385471c85ff46f6cea1424bb17303a7763
Abhigyan Tripathi <abhigyantrips@gmail.com>
2021-12-08 22:46:04 +0530

    Update license year, ownership in all files.

--- disnake/ext/commands/common_bot_base.py
+++ disnake/ext/commands/common_bot_base.py
@@ -1,6 +1,7 @@
 # The MIT License (MIT)
 
-# Copyright (c) 2015-present Rapptz, EQUENOS
+# Copyright (c) 2015-2021 Rapptz
+Copyright (c) 2021-present DisnakeDev, EQUENOS
 
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
