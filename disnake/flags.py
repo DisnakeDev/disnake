52ae4ae118da3170e5230f587f9e5611e259f545
Victorsitou <67214928+Victorsitou@users.noreply.github.com>
2021-12-12 23:55:12 -0300

    Add `SPAMMER` user flag.

--- disnake/flags.py
+++ disnake/flags.py
@@ -448,6 +448,11 @@ class PublicUserFlags(BaseFlags):
         """
         return UserFlags.http_interactions_bot.value
 
+    @flag_value
+    def spammer(self):
+        """:class:`bool`: Returns ``True`` if the user is a spammer."""
+        return UserFlags.spammer.value
+
     def all(self) -> List[UserFlags]:
         """List[:class:`UserFlags`]: Returns all public flags the user has."""
         return [public_flag for public_flag in UserFlags if self._has_flag(public_flag.value)]
