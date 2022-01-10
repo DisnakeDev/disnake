a9dc66cb0b1305670557dbb3dd23d8bc37599192
EQUENOS <50338932+EQUENOS@users.noreply.github.com>
2022-01-11 00:13:29 +0300

    Fixed a rare command sync bug (#254)
    * Fixed a rare command sync bug
    
    It was impossible to register commands of different types with the same name. The reason was one of the internal functions that compares old and new commands. It mapped them like {name: cmd} instead of {(name, type): cmd}
    
    * Remove "Type migration" because it's no longer an issue
    
    Imagine we have command A of one type and command B of another type but with the same name.
    Previously, API didn't allow to overwrite A with B without forcing users to make an extra step of deleting A and inserting B afterwards.
    
    * Fixed formatting

--- disnake/ext/commands/interaction_bot_base.py
+++ disnake/ext/commands/interaction_bot_base.py
@@ -94,23 +94,20 @@ def _app_commands_diff(
     new_commands: Iterable[ApplicationCommand],
     old_commands: Iterable[ApplicationCommand],
 ) -> Dict[str, List[ApplicationCommand]]:
-    new_cmds = {cmd.name: cmd for cmd in new_commands}
-    old_cmds = {cmd.name: cmd for cmd in old_commands}
+    new_cmds = {(cmd.name, cmd.type): cmd for cmd in new_commands}
+    old_cmds = {(cmd.name, cmd.type): cmd for cmd in old_commands}
 
     diff = {
         "no_changes": [],
         "upsert": [],
         "edit": [],
         "delete": [],
-        "change_type": [],
     }
 
-    for name, new_cmd in new_cmds.items():
-        old_cmd = old_cmds.get(name)
+    for name_and_type, new_cmd in new_cmds.items():
+        old_cmd = old_cmds.get(name_and_type)
         if old_cmd is None:
             diff["upsert"].append(new_cmd)
-        elif old_cmd.type != new_cmd.type:
-            diff["change_type"].append(new_cmd)
         elif new_cmd._always_synced:
             diff["no_changes"].append(old_cmd)
             continue
@@ -119,8 +116,8 @@ def _app_commands_diff(
         else:
             diff["no_changes"].append(new_cmd)
 
-    for name, old_cmd in old_cmds.items():
-        if name not in new_cmds:
+    for name_and_type, old_cmd in old_cmds.items():
+        if name_and_type not in new_cmds:
             diff["delete"].append(old_cmd)
 
     return diff
@@ -130,7 +127,6 @@ def _show_diff(diff: Dict[str, List[ApplicationCommand]], line_prefix: str = "")
     to_upsert = f",\n{line_prefix}    ".join(str(cmd) for cmd in diff["upsert"]) or "-"
     to_edit = f",\n{line_prefix}    ".join(str(cmd) for cmd in diff["edit"]) or "-"
     to_delete = f",\n{line_prefix}    ".join(str(cmd) for cmd in diff["delete"]) or "-"
-    change_type = f",\n{line_prefix}    ".join(str(cmd) for cmd in diff["change_type"]) or "-"
     no_changes = f",\n{line_prefix}    ".join(str(cmd) for cmd in diff["no_changes"]) or "-"
     print(
         f"{line_prefix}To upsert:",
@@ -139,8 +135,6 @@ def _show_diff(diff: Dict[str, List[ApplicationCommand]], line_prefix: str = "")
         f"    {to_edit}",
         f"To delete:",
         f"    {to_delete}",
-        f"Type migration:",
-        f"    {change_type}",
         f"No changes:",
         f"    {no_changes}",
         sep=f"\n{line_prefix}",
@@ -681,12 +675,7 @@ class InteractionBotBase(CommonBotBase):
         diff = _app_commands_diff(
             global_cmds, self._connection._global_application_commands.values()
         )
-        update_required = (
-            bool(diff["upsert"])
-            or bool(diff["edit"])
-            or bool(diff["change_type"])
-            or bool(diff["delete"])
-        )
+        update_required = bool(diff["upsert"]) or bool(diff["edit"]) or bool(diff["delete"])
 
         if self._sync_commands_debug:
             # Show the difference
@@ -702,13 +691,7 @@ class InteractionBotBase(CommonBotBase):
         if update_required:
             # Notice that we don't do any API requests if there're no changes.
             try:
-                to_send = diff["no_changes"] + diff["edit"]
-                if bool(diff["change_type"]):
-                    # We can't just "edit" the command type, so we bulk delete the commands with old type first.
-                    await self.bulk_overwrite_global_commands(to_send)
-                # Finally, we make an API call that applies all changes from the code.
-                to_send.extend(diff["upsert"])
-                to_send.extend(diff["change_type"])
+                to_send = diff["no_changes"] + diff["edit"] + diff["upsert"]
                 await self.bulk_overwrite_global_commands(to_send)
             except Exception as e:
                 warnings.warn(f"Failed to overwrite global commands due to {e}", SyncWarning)
@@ -718,12 +701,7 @@ class InteractionBotBase(CommonBotBase):
         for guild_id, cmds in guild_cmds.items():
             current_guild_cmds = self._connection._guild_application_commands.get(guild_id, {})
             diff = _app_commands_diff(cmds, current_guild_cmds.values())
-            update_required = (
-                bool(diff["upsert"])
-                or bool(diff["edit"])
-                or bool(diff["change_type"])
-                or bool(diff["delete"])
-            )
+            update_required = bool(diff["upsert"]) or bool(diff["edit"]) or bool(diff["delete"])
             # Show diff
             if self._sync_commands_debug:
                 print(
@@ -735,11 +713,7 @@ class InteractionBotBase(CommonBotBase):
             # Do API requests and cache
             if update_required:
                 try:
-                    to_send = diff["no_changes"] + diff["edit"]
-                    if bool(diff["change_type"]):
-                        await self.bulk_overwrite_guild_commands(guild_id, to_send)
-                    to_send.extend(diff["upsert"])
-                    to_send.extend(diff["change_type"])
+                    to_send = diff["no_changes"] + diff["edit"] + diff["upsert"]
                     await self.bulk_overwrite_guild_commands(guild_id, to_send)
                 except Exception as e:
                     warnings.warn(
