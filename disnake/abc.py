8518bc84853c10a4f19adce382c70b5f6e49dc42
shiftinv <8530778+shiftinv@users.noreply.github.com>
2022-01-18 23:38:46 +0000

    Fix lint issues

--- disnake/abc.py
+++ disnake/abc.py
@@ -719,70 +719,70 @@ class GuildChannel(ABC):
     async def set_permissions(self, target, *, overwrite=_undefined, reason=None, **permissions):
         r"""|coro|
 
-         Sets the channel specific permission overwrites for a target in the
-         channel.
+        Sets the channel specific permission overwrites for a target in the
+        channel.
 
-         The ``target`` parameter should either be a :class:`.Member` or a
-         :class:`.Role` that belongs to guild.
+        The ``target`` parameter should either be a :class:`.Member` or a
+        :class:`.Role` that belongs to guild.
 
-         The ``overwrite`` parameter, if given, must either be ``None`` or
-         :class:`.PermissionOverwrite`. For convenience, you can pass in
-         keyword arguments denoting :class:`.Permissions` attributes. If this is
-         done, then you cannot mix the keyword arguments with the ``overwrite``
-         parameter.
+        The ``overwrite`` parameter, if given, must either be ``None`` or
+        :class:`.PermissionOverwrite`. For convenience, you can pass in
+        keyword arguments denoting :class:`.Permissions` attributes. If this is
+        done, then you cannot mix the keyword arguments with the ``overwrite``
+        parameter.
 
-         If the ``overwrite`` parameter is ``None``, then the permission
-         overwrites are deleted.
+        If the ``overwrite`` parameter is ``None``, then the permission
+        overwrites are deleted.
 
-         You must have :attr:`.Permissions.manage_roles` permission to do this.
+        You must have :attr:`.Permissions.manage_roles` permission to do this.
 
-         .. note::
+        .. note::
 
-             This method *replaces* the old overwrites with the ones given.
+            This method *replaces* the old overwrites with the ones given.
 
-         Examples
-         ----------
+        Examples
+        ----------
 
-         Setting allow and deny: ::
+        Setting allow and deny: ::
 
-             await message.channel.set_permissions(message.author, read_messages=True,
-                                                                   send_messages=False)
+            await message.channel.set_permissions(message.author, read_messages=True,
+                                                                  send_messages=False)
 
-         Deleting overwrites ::
+        Deleting overwrites ::
 
-             await channel.set_permissions(member, overwrite=None)
+            await channel.set_permissions(member, overwrite=None)
 
-         Using :class:`.PermissionOverwrite` ::
+        Using :class:`.PermissionOverwrite` ::
 
-             overwrite = disnake.PermissionOverwrite()
-             overwrite.send_messages = False
-             overwrite.read_messages = True
-             await channel.set_permissions(member, overwrite=overwrite)
+            overwrite = disnake.PermissionOverwrite()
+            overwrite.send_messages = False
+            overwrite.read_messages = True
+            await channel.set_permissions(member, overwrite=overwrite)
 
-         Parameters
-         -----------
-         target: Union[:class:`.Member`, :class:`.Role`]
-             The member or role to overwrite permissions for.
-         overwrite: Optional[:class:`.PermissionOverwrite`]
-             The permissions to allow and deny to the target, or ``None`` to
-             delete the overwrite.
-         \*\*permissions
-             A keyword argument list of permissions to set for ease of use.
-             Cannot be mixed with ``overwrite``.
-         reason: Optional[:class:`str`]
-             The reason for doing this action. Shows up on the audit log.
+        Parameters
+        -----------
+        target: Union[:class:`.Member`, :class:`.Role`]
+            The member or role to overwrite permissions for.
+        overwrite: Optional[:class:`.PermissionOverwrite`]
+            The permissions to allow and deny to the target, or ``None`` to
+            delete the overwrite.
+        \*\*permissions
+            A keyword argument list of permissions to set for ease of use.
+            Cannot be mixed with ``overwrite``.
+        reason: Optional[:class:`str`]
+            The reason for doing this action. Shows up on the audit log.
 
-         Raises
-         -------
-         Forbidden
-             You do not have permissions to edit channel specific permissions.
-         HTTPException
-             Editing channel specific permissions failed.
-         NotFound
-             The role or member being edited is not part of the guild.
-         InvalidArgument
-             The overwrite parameter invalid or the target type was not
-             :class:`.Role` or :class:`.Member`.
+        Raises
+        -------
+        Forbidden
+            You do not have permissions to edit channel specific permissions.
+        HTTPException
+            Editing channel specific permissions failed.
+        NotFound
+            The role or member being edited is not part of the guild.
+        InvalidArgument
+            The overwrite parameter invalid or the target type was not
+            :class:`.Role` or :class:`.Member`.
         """
         http = self._state.http
 
