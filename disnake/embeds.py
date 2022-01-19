d5e21ff7a85c626a0b18f2f7b0d5f4f4f1801bde
onerandomusername <genericusername414+git@gmail.com>
2022-01-19 12:12:08 -0500

    fix english

--- disnake/embeds.py
+++ disnake/embeds.py
@@ -149,7 +149,7 @@ class Embed:
     type: :class:`str`
         The type of embed. Usually "rich".
         This can be set during initialisation.
-        Possible strings for embed types can be found on disnake's
+        Possible strings for embed types can be found on discord's
         `api docs <https://discord.com/developers/docs/resources/channel#embed-object-embed-types>`_
     description: :class:`str`
         The description of the embed.
