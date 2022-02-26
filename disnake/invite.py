e56fb56dfeceeb719225e3efaaf1d4687593ad55
onerandomusername <genericusername414+git@gmail.com>
2022-02-25 20:01:49 -0500

    fix: make PartialInviteChannel.__str__ always return a string
    channel name can be nullable

--- disnake/invite.py
+++ disnake/invite.py
@@ -85,7 +85,7 @@ class PartialInviteChannel:
 
     Attributes
     ----------
-    name: :class:`str`
+    name: Optional[:class:`str`]
         The partial channel's name.
     id: :class:`int`
         The partial channel's ID.
@@ -97,11 +97,13 @@ class PartialInviteChannel:
 
     def __init__(self, data: InviteChannelPayload):
         self.id: int = int(data["id"])
-        self.name: str = data["name"]
+        self.name: Optional[str] = data.get("name")
         self.type: ChannelType = try_enum(ChannelType, data["type"])
 
     def __str__(self) -> str:
-        return self.name
+        if self.type is ChannelType.group:
+            return self.name or "Unnamed"
+        return self.name or ""
 
     def __repr__(self) -> str:
         return f"<PartialInviteChannel id={self.id} name={self.name} type={self.type!r}>"
