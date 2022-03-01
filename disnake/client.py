b9795ccf99540468dc9ba9ee7876ec3fed345344
arl <genericusername414+git@gmail.com>
2022-03-01 00:38:48 -0500

    fix: use the correct query parameter
    Co-authored-by: shiftinv <8530778+shiftinv@users.noreply.github.com>

--- disnake/client.py
+++ disnake/client.py
@@ -1738,7 +1738,7 @@ class Client:
         List[:class:`.DiscoveryCategory`]
             A list of all discovery categories.
         """
-        payload = {"is_primary": primary}
+        payload = {"primary_only": primary}
 
         if locale is not MISSING:
             payload["locale"] = locale  # type: ignore
