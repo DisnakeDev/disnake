ce71d160b39a46728520c33d6e7b16e13c536c44
shiftinv <me@shiftinv.cc>
2022-03-02 23:58:33 +0100

    chore: update pyright to 1.1.226

--- disnake/ext/commands/core.py
+++ disnake/ext/commands/core.py
@@ -1338,7 +1338,7 @@ class GroupMixin(Generic[CogT]):
     ) -> Callable[[CommandCallback[CogT, ContextT, P, T]], CommandT]:
         ...
 
-    def command(  # type: ignore
+    def command(
         self,
         name: str = MISSING,
         cls: Union[Type[Command[CogT, P, T]], Type[CommandT]] = MISSING,
@@ -1384,7 +1384,7 @@ class GroupMixin(Generic[CogT]):
     ) -> Callable[[CommandCallback[CogT, ContextT, P, T]], GroupT]:
         ...
 
-    def group(  # type: ignore
+    def group(
         self,
         name: str = MISSING,
         cls: Union[Type[Group[CogT, P, T]], Type[GroupT]] = MISSING,
@@ -1544,8 +1544,7 @@ def command(
     ...
 
 
-# ignored since the order in the Union in CommandCallback matters here for some reason
-def command(  # type: ignore
+def command(
     name: str = MISSING,
     cls: Union[Type[Command[CogT, P, T]], Type[CommandT]] = MISSING,
     **attrs: Any,
@@ -1610,7 +1609,7 @@ def group(
     ...
 
 
-def group(  # type: ignore  # see command above
+def group(
     name: str = MISSING,
     cls: Union[Type[Group[CogT, P, T]], Type[GroupT]] = MISSING,
     **attrs: Any,
