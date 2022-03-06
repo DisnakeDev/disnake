68d12a50d74f5eb92e7a5c5932c0d4a8795a3376
shiftinv <8530778+shiftinv@users.noreply.github.com>
2022-03-06 20:10:14 +0100

    fix(converter): move import for `commands.clean_content` (#396)
    * chore: use _utils_get in converters
    
    * fix: move import to be accessible at runtime
    
    * fix: improve clean_content typing

--- disnake/ext/commands/converter.py
+++ disnake/ext/commands/converter.py
@@ -48,12 +48,13 @@ from typing import (
 
 import disnake
 
+from .context import Context
 from .errors import *
 
 if TYPE_CHECKING:
     from disnake.message import MessageableChannel
 
-    from .context import Context
+    # TODO: don't use unbound generic `Context`
     from .core import AnyContext
 
 # TODO: USE ACTUAL FUNCTIONS INSTEAD OF USELESS CLASSES
@@ -207,7 +208,7 @@ class MemberConverter(IDConverter[disnake.Member]):
         if len(argument) > 5 and argument[-5] == "#":
             username, _, discriminator = argument.rpartition("#")
             members = await guild.query_members(username, limit=100, cache=cache)
-            return disnake.utils.get(members, name=username, discriminator=discriminator)
+            return _utils_get(members, name=username, discriminator=discriminator)
         else:
             members = await guild.query_members(argument, limit=100, cache=cache)
             return disnake.utils.find(lambda m: m.name == argument or m.nick == argument, members)
@@ -456,7 +457,7 @@ class GuildChannelConverter(IDConverter[disnake.abc.GuildChannel]):
             # not a mention
             if guild:
                 iterable: Iterable[CT] = getattr(guild, attribute)
-                result = disnake.utils.get(iterable, name=argument)
+                result = _utils_get(iterable, name=argument)
             else:
                 result = disnake.utils.find(
                     lambda c: isinstance(c, type) and c.name == argument, bot.get_all_channels()
@@ -483,7 +484,7 @@ class GuildChannelConverter(IDConverter[disnake.abc.GuildChannel]):
             # not a mention
             if guild:
                 iterable: Iterable[TT] = getattr(guild, attribute)
-                result = disnake.utils.get(iterable, name=argument)
+                result = _utils_get(iterable, name=argument)
         else:
             thread_id = int(match.group(1))
             if guild:
@@ -734,7 +735,7 @@ class RoleConverter(IDConverter[disnake.Role]):
         if match:
             result = guild.get_role(int(match.group(1)))
         else:
-            result = disnake.utils.get(guild._roles.values(), name=argument)
+            result = _utils_get(guild._roles.values(), name=argument)
 
         if result is None:
             raise RoleNotFound(argument)
@@ -785,7 +786,7 @@ class GuildConverter(IDConverter[disnake.Guild]):
             result = bot.get_guild(guild_id)
 
         if result is None:
-            result = disnake.utils.get(bot.guilds, name=argument)
+            result = _utils_get(bot.guilds, name=argument)
 
             if result is None:
                 raise GuildNotFound(argument)
@@ -819,10 +820,10 @@ class EmojiConverter(IDConverter[disnake.Emoji]):
         if match is None:
             # Try to get the emoji by name. Try local guild first.
             if guild:
-                result = disnake.utils.get(guild.emojis, name=argument)
+                result = _utils_get(guild.emojis, name=argument)
 
             if result is None:
-                result = disnake.utils.get(bot.emojis, name=argument)
+                result = _utils_get(bot.emojis, name=argument)
         else:
             # Try to look up emoji by id.
             result = bot.get_emoji(int(match.group(1)))
@@ -880,10 +881,10 @@ class GuildStickerConverter(IDConverter[disnake.GuildSticker]):
         if match is None:
             # Try to get the sticker by name. Try local guild first.
             if guild:
-                result = disnake.utils.get(guild.stickers, name=argument)
+                result = _utils_get(guild.stickers, name=argument)
 
             if result is None:
-                result = disnake.utils.get(bot.stickers, name=argument)
+                result = _utils_get(bot.stickers, name=argument)
         else:
             # Try to look up sticker by id.
             result = bot.get_sticker(int(match.group(1)))
@@ -974,9 +975,10 @@ class clean_content(Converter[str]):
 
     async def convert(self, ctx: AnyContext, argument: str) -> str:
         msg = ctx.message if isinstance(ctx, Context) else None
+        bot: disnake.Client = ctx.bot
 
         def resolve_user(id: int) -> str:
-            m = (msg and _utils_get(msg.mentions, id=id)) or ctx.bot.get_user(id)
+            m = (msg and _utils_get(msg.mentions, id=id)) or bot.get_user(id)
             if m is None and ctx.guild:
                 m = ctx.guild.get_member(id)
             return f"@{m.display_name if self.use_nicknames else m.name}" if m else "@deleted-user"
