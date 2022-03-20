17e565619fec707170c983b224ca393ae315924b
shiftinv <me@shiftinv.cc>
2022-03-20 20:46:23 +0100

    fix: rebind cached guilds/channels of cached messages after receiving guilds
    This was previously done in the `READY` handler, which only receives guilds in the `unavailable` state.
    These guilds are effectively empty, which resulted in all cached messages' channels references being rebound
    to `Object`s instead of proper channels.
    To solve this, references will now be rebound only once all `GUILD_CREATE` events were received.

--- disnake/state.py
+++ disnake/state.py
@@ -1956,9 +1956,9 @@ class AutoShardedConnectionState(ConnectionState):
         self.shard_ids: Union[List[int], range] = []
         self.shards_launched: asyncio.Event = asyncio.Event()
 
-    def _update_message_references(self) -> None:
-        # self._messages won't be None when this is called
-        for msg in self._messages:  # type: ignore
+    def _update_guild_channel_references(self) -> None:
+        messages: Sequence[Message] = self._messages or []
+        for msg in messages:
             if not msg.guild:
                 continue
 
@@ -1966,7 +1966,7 @@ class AutoShardedConnectionState(ConnectionState):
             if new_guild is not None and new_guild is not msg.guild:
                 channel_id = msg.channel.id
                 channel = new_guild._resolve_channel(channel_id) or Object(id=channel_id)
-                # channel will either be a TextChannel, Thread or Object
+                # channel will either be a TextChannel, VoiceChannel, Thread or Object
                 msg._rebind_cached_references(new_guild, channel)  # type: ignore
 
     async def chunker(
@@ -2023,6 +2023,9 @@ class AutoShardedConnectionState(ConnectionState):
                     future.set_result([])
 
                 processed.append((guild, future))
+
+        # update references once the guild cache is repopulated
+        self._update_guild_channel_references()
 
         guilds = sorted(processed, key=lambda g: g[0].shard_id)
         for shard_id, info in itertools.groupby(guilds, key=lambda g: g[0].shard_id):
@@ -2086,9 +2089,6 @@ class AutoShardedConnectionState(ConnectionState):
 
         for guild_data in data["guilds"]:
             self._add_guild_from_data(guild_data)
-
-        if self._messages:
-            self._update_message_references()
 
         self.dispatch("connect")
         self.dispatch("shard_connect", data["__shard_id__"])
