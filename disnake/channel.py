71d26da10de34286c1c55b12e0fb98acd68a9008
shiftinv <8530778+shiftinv@users.noreply.github.com>
2022-03-29 22:51:07 +0200

    fix: improve `PartialMessageable` channel handling (#426)

--- disnake/channel.py
+++ disnake/channel.py
@@ -61,7 +61,6 @@ from .enums import (
 from .errors import ClientException, InvalidArgument
 from .iterators import ArchivedThreadIterator
 from .mixins import Hashable
-from .object import Object
 from .permissions import PermissionOverwrite, Permissions
 from .stage_instance import StageInstance
 from .threads import Thread
@@ -2314,12 +2313,11 @@ class PartialMessageable(disnake.abc.Messageable, Hashable):
 
     def __init__(self, state: ConnectionState, id: int, type: Optional[ChannelType] = None):
         self._state: ConnectionState = state
-        self._channel: Object = Object(id=id)
         self.id: int = id
         self.type: Optional[ChannelType] = type
 
-    async def _get_channel(self) -> Object:
-        return self._channel
+    async def _get_channel(self) -> PartialMessageable:
+        return self
 
     def get_partial_message(self, message_id: int, /) -> PartialMessage:
         """Creates a :class:`PartialMessage` from the given message ID.
