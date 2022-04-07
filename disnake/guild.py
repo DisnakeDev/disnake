3fa1ec86d954807992f72984ce1fdea96f556d5f
shiftinv <me@shiftinv.cc>
2022-04-07 22:46:17 +0200

    fix: remove `COMMERCE` guild feature

--- disnake/guild.py
+++ disnake/guild.py
@@ -204,7 +204,6 @@ class Guild(Hashable):
         - ``ANIMATED_BANNER``: Guild can upload an animated banner.
         - ``ANIMATED_ICON``: Guild can upload an animated icon.
         - ``BANNER``: Guild can upload and use a banner. (i.e. :attr:`.banner`)
-        - ``COMMERCE``: Guild can sell things using store channels.
         - ``COMMUNITY``: Guild is a community server.
         - ``DISCOVERABLE``: Guild shows up in Server Discovery.
         - ``ENABLED_DISCOVERABLE_BEFORE``: Guild had Server Discovery enabled at least once.
