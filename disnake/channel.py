26bf3a9fbe7c8429feb7891b86bafdddb6eb0ba2
shiftinv <8530778+shiftinv@users.noreply.github.com>
2022-04-09 13:41:33 +0200

    feat: add stage instance start notification (#440)
    * feat: add stage instance start notification
    
    * chore: rename parameter to `notify_everyone`
    
    * chore: add `versionadded` directive

--- disnake/channel.py
+++ disnake/channel.py
@@ -1404,6 +1404,7 @@ class StageChannel(VocalGuildChannel):
         *,
         topic: str,
         privacy_level: StagePrivacyLevel = MISSING,
+        notify_everyone: bool = False,
         reason: Optional[str] = None,
     ) -> StageInstance:
         """|coro|
@@ -1423,6 +1424,12 @@ class StageChannel(VocalGuildChannel):
             The stage instance's privacy level. Defaults to :attr:`StagePrivacyLevel.guild_only`.
         reason: :class:`str`
             The reason the stage instance was created. Shows up on the audit log.
+        notify_everyone: :class:`bool`
+            Whether to notify ``@everyone`` that the stage instance has started.
+            Requires the :attr:`~Permissions.mention_everyone` permission on the stage channel.
+            Defaults to ``False``.
+
+            .. versionadded:: 2.5
 
         Raises
         ------
@@ -1438,7 +1445,11 @@ class StageChannel(VocalGuildChannel):
         :class:`StageInstance`
             The newly created stage instance.
         """
-        payload: Dict[str, Any] = {"channel_id": self.id, "topic": topic}
+        payload: Dict[str, Any] = {
+            "channel_id": self.id,
+            "topic": topic,
+            "send_start_notification": notify_everyone,
+        }
 
         if privacy_level is not MISSING:
             if not isinstance(privacy_level, StagePrivacyLevel):
--- disnake/http.py
+++ disnake/http.py
@@ -1896,6 +1896,7 @@ class HTTPClient:
             "channel_id",
             "topic",
             "privacy_level",
+            "send_start_notification",
         )
         payload = {k: v for k, v in payload.items() if k in valid_keys}
 
