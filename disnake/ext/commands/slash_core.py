5f22ae552d9609c277612be61571740f5d7f67ef
shiftinv <8530778+shiftinv@users.noreply.github.com>
2022-04-14 22:06:50 +0200

    fix: improve validation of application command/option names (#422)
    Co-authored-by: arl <genericusername414+git@gmail.com>

--- disnake/ext/commands/slash_core.py
+++ disnake/ext/commands/slash_core.py
@@ -133,7 +133,6 @@ class SubCommandGroup(InvokableApplicationCommand):
         self.option = Option(
             name=self.name, description="-", type=OptionType.sub_command_group, options=[]
         )
-        self.name = self.option.name
         self.qualified_name: str = ""
 
     @property
@@ -232,7 +231,6 @@ class SubCommand(InvokableApplicationCommand):
             type=OptionType.sub_command,
             options=options,
         )
-        self.name = self.option.name
         self.qualified_name = ""
 
     @property
@@ -349,8 +347,6 @@ class InvokableSlashCommand(InvokableApplicationCommand):
             options=options or [],
             default_permission=default_permission,
         )
-        # `SlashCommand.__init__` converts names to lowercase, need to use that name here as well
-        self.qualified_name = self.name = self.body.name
 
     @property
     def description(self) -> str:
