754391f82e2af76b37480b09fd71c4490dde898b
Victor <67214928+Victorsitou@users.noreply.github.com>
2022-04-13 20:10:03 -0400

    docs: int not bool
    duh

--- disnake/channel.py
+++ disnake/channel.py
@@ -2285,7 +2285,7 @@ class ForumChannel(disnake.abc.GuildChannel, Hashable):
 
         Parameters
         ----------
-        limit: Optional[:class:`bool`]
+        limit: Optional[:class:`int`]
             The number of threads to retrieve.
             If ``None``, retrieves every archived thread in the channel. Note, however,
             that this would make it a slow operation.
