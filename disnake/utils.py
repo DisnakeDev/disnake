07fe393ded25d2db34b4b3ff0b57164325bce243
shiftinv <me@shiftinv.cc>
2022-04-28 01:53:06 +0200

    fix: support memoryview in mimetype check

--- disnake/utils.py
+++ disnake/utils.py
@@ -509,13 +509,13 @@ def _maybe_cast(value: V, converter: Callable[[V], T], default: T = None) -> Opt
 
 
 def _get_mime_type_for_image(data: bytes):
-    if data.startswith(b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A"):
+    if data[0:8] == b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A":
         return "image/png"
     elif data[0:3] == b"\xff\xd8\xff" or data[6:10] in (b"JFIF", b"Exif"):
         return "image/jpeg"
-    elif data.startswith((b"\x47\x49\x46\x38\x37\x61", b"\x47\x49\x46\x38\x39\x61")):
+    elif data[0:6] in (b"\x47\x49\x46\x38\x37\x61", b"\x47\x49\x46\x38\x39\x61"):
         return "image/gif"
-    elif data.startswith(b"RIFF") and data[8:12] == b"WEBP":
+    elif data[0:4] == b"RIFF" and data[8:12] == b"WEBP":
         return "image/webp"
     else:
         raise InvalidArgument("Unsupported image type given")
