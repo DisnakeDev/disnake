98f0914f02ce412fda73ff8166d37c6d7cd7b24d
Chromosomologist <55550164+Chromosomologist@users.noreply.github.com>
2022-05-05 02:19:16 +0200

    docs: ungoof

--- disnake/ext/commands/cog.py
+++ disnake/ext/commands/cog.py
@@ -116,7 +116,7 @@ class CogMeta(type):
 
         .. versionadded:: 1.6
 
-    command_attrs: :class:`dict`
+    command_attrs: Dict[:class:`str`, Any]
         A list of attributes to apply to every command inside this cog. The dictionary
         is passed into the :class:`Command` options at ``__init__``.
         If you specify attributes inside the command attribute in the class, it will
@@ -133,26 +133,26 @@ class CogMeta(type):
                 async def bar(self, ctx):
                     pass # hidden -> False
 
-    slash_command_attrs: :class:`dict`
+    slash_command_attrs: Dict[:class:`str`, Any]
         A list of attributes to apply to every slash command inside this cog. The dictionary
-        is passed into the :class:`InvokableSlashCommand`s' options at ``__init__``. Usage of
-        this kwarg is otherwise the same as with `command_attrs`.
+        is passed into the options of every :class:`InvokableSlashCommand` at ``__init__``.
+        Usage of this kwarg is otherwise the same as with ``command_attrs``.
 
-        .. note:: This does not apply to :class:`SubCommand`s or `SubCommandGroup`s
+        .. note:: This does not apply to instances of :class:`SubCommand` or :class:`SubCommandGroup`.
 
         .. versionadded:: 2.5
 
-    user_command_attrs: :class:`dict`
+    user_command_attrs: Dict[:class:`str`, Any]
         A list of attributes to apply to every user command inside this cog. The dictionary
-        is passed into the :class:`InvokableUserCommand`s' options at ``__init__``. Usage of this
-        kwarg is otherwise the same as with `command_attrs`.
+        is passed into the options of every :class:`InvokableUserCommand` at ``__init__``.
+        Usage of this kwarg is otherwise the same as with ``command_attrs``.
 
         .. versionadded:: 2.5
 
-    message_command_attrs: :class:`dict`
+    message_command_attrs: Dict[:class:`str`, Any]
         A list of attributes to apply to every message command inside this cog. The dictionary
-        is passed into the :class:`InvokableMessageCommand`s' options at ``__init__``. Usage of
-        this kwarg is otherwise the same as with `command_attrs`.
+        is passed into the options of every :class:`InvokableMessageCommand` at ``__init__``.
+        Usage of this kwarg is otherwise the same as with ``command_attrs``.
 
         .. versionadded:: 2.5
     """
