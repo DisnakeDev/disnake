5796448813248f498529964d62b48ee967a78118
shiftinv <me@shiftinv.cc>
2022-05-27 18:40:54 +0200

    fix: improve userecho example, fix avatar

--- examples/user_echo_webhook.py
+++ examples/user_echo_webhook.py
@@ -24,27 +24,27 @@ async def on_ready():
 @commands.has_permissions(
     administrator=True
 )  # To make sure that not everyone can use this command.
-async def userecho(ctx, member: disnake.Member, *, content):
+async def userecho(ctx: commands.Context, member: disnake.Member, *, content):
 
     await ctx.message.delete()  # We don't want users to see who initiated the command, to make it more realistic :P
 
     # We fetch the channel's webhooks.
-    channel_webhooks = await ctx.message.channel.webhooks()
+    channel = ctx.channel
+    if not isinstance(channel, disnake.TextChannel):
+        return
+    channel_webhooks = await channel.webhooks()
 
     # We check if the bot's webhook already exists in the channel.
     for webhook in channel_webhooks:
         # We will check if the creator of the webhook is the same as the bot, and if the name is the same.
         if webhook.user == client.user and webhook.name == "Bot Webhook":
-            await webhook.send(
-                content=content, username=member.display_name, avatar_url=member.avatar
-            )
-            return  # The program will not go further.
+            break
+    else:
+        # If the webhook does not exist, it will be created.
+        webhook = await channel.create_webhook(name="Bot Webhook")
 
-    # If the webhook does not exist, it will be created.
-    new_webhook = await ctx.channel.create_webhook(name="Bot Webhook", reason="Bot Webhook")
-
-    # Finally, sending the message via the webhook - the bot will fetch the user's display name and avatar.
-    await new_webhook.send(
+    # Finally, sending the message via the webhook, using the user's display name and avatar.
+    await webhook.send(
         content=content, username=member.display_name, avatar_url=member.display_avatar.url
     )
 
