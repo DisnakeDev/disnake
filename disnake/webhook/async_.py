47836860ef34d172d2e289e1999d41148f00b05a
onerandomusername <genericusername414+git@gmail.com>
2022-05-31 19:37:36 -0400

    fix: properly have WebhookMessage.channel return the correct channel

--- disnake/webhook/async_.py
+++ disnake/webhook/async_.py
@@ -1401,7 +1401,10 @@ class Webhook(BaseWebhook):
     def _create_message(self, data):
         state = _WebhookState(self, parent=self._state)
         # state may be artificial (unlikely at this point...)
-        channel = self.channel or PartialMessageable(state=self._state, id=int(data["channel_id"]))  # type: ignore
+        channel = self.channel
+        channel_id = int(data["channel_id"])
+        if not channel or channel.id != channel_id:
+            channel = PartialMessageable(state=self._state, id=channel_id)  # type: ignore
         # state is artificial
         return WebhookMessage(data=data, state=state, channel=channel)  # type: ignore
 
