9b9f5c0263c3b26037ca9488b1057eceb8a0d95e
shiftinv <8530778+shiftinv@users.noreply.github.com>
2022-06-11 02:45:08 +0200

    fix!: rename scheduled event parameter `channel_id` to `channel`, fix validation (#548)
    * fix: rename scheduled event parameter to `channel`
    
    * feat: add overloads to create/edit
    
    * fix: make `scheduled_end_time` on edit optional
    
    * fix: remove somewhat faulty validation of scheduled event edits
    
    * fix: set `channel_id=None` when changing type to external
    
    * fix: make `scheduled_end_time` parameter optional
    
    * docs: update entity type requirements
    
    * docs: add versionadded/versionchanged
    
    * fix: remove some optionals from overload parameters

--- disnake/guild_scheduled_event.py
+++ disnake/guild_scheduled_event.py
@@ -25,7 +25,7 @@ DEALINGS IN THE SOFTWARE.
 from __future__ import annotations
 
 from datetime import datetime
-from typing import TYPE_CHECKING, Any, Dict, Optional
+from typing import TYPE_CHECKING, Any, Dict, Literal, Optional, overload
 
 from .asset import Asset
 from .enums import (
@@ -261,16 +261,77 @@ class GuildScheduledEvent(Hashable):
         """
         await self._state.http.delete_guild_scheduled_event(self.guild_id, self.id)
 
+    # note: unlike `Guild.create_scheduled_event`, we don't remove `entity_type`-specific defaults
+    # from parameters in the overloads here, as the entity_type might be the same as before,
+    # in which case those parameters don't have to be passed
+
+    # no entity_type specified
+    @overload
+    async def edit(
+        self,
+        *,
+        name: str = ...,
+        description: Optional[str] = ...,
+        image: Optional[AssetBytes] = ...,
+        channel: Optional[Snowflake] = ...,
+        privacy_level: GuildScheduledEventPrivacyLevel = ...,
+        scheduled_start_time: datetime = ...,
+        scheduled_end_time: Optional[datetime] = ...,
+        entity_metadata: Optional[GuildScheduledEventMetadata] = ...,
+        status: GuildScheduledEventStatus = ...,
+        reason: Optional[str] = ...,
+    ) -> GuildScheduledEvent:
+        ...
+
+    # new entity_type is `external`, no channel
+    @overload
+    async def edit(
+        self,
+        *,
+        entity_type: Literal[GuildScheduledEventEntityType.external],
+        name: str = ...,
+        description: Optional[str] = ...,
+        image: Optional[AssetBytes] = ...,
+        privacy_level: GuildScheduledEventPrivacyLevel = ...,
+        scheduled_start_time: datetime = ...,
+        scheduled_end_time: datetime = ...,
+        entity_metadata: GuildScheduledEventMetadata = ...,
+        status: GuildScheduledEventStatus = ...,
+        reason: Optional[str] = ...,
+    ) -> GuildScheduledEvent:
+        ...
+
+    # new entity_type is `voice` or `stage_instance`, no entity_metadata
+    @overload
+    async def edit(
+        self,
+        *,
+        entity_type: Literal[
+            GuildScheduledEventEntityType.voice,
+            GuildScheduledEventEntityType.stage_instance,
+        ],
+        name: str = ...,
+        description: Optional[str] = ...,
+        image: Optional[AssetBytes] = ...,
+        channel: Snowflake = ...,
+        privacy_level: GuildScheduledEventPrivacyLevel = ...,
+        scheduled_start_time: datetime = ...,
+        scheduled_end_time: Optional[datetime] = ...,
+        status: GuildScheduledEventStatus = ...,
+        reason: Optional[str] = ...,
+    ) -> GuildScheduledEvent:
+        ...
+
     async def edit(
         self,
         *,
         name: str = MISSING,
         description: Optional[str] = MISSING,
         image: Optional[AssetBytes] = MISSING,
-        channel_id: Optional[int] = MISSING,
+        channel: Optional[Snowflake] = MISSING,
         privacy_level: GuildScheduledEventPrivacyLevel = MISSING,
         scheduled_start_time: datetime = MISSING,
-        scheduled_end_time: datetime = MISSING,
+        scheduled_end_time: Optional[datetime] = MISSING,
         entity_type: GuildScheduledEventEntityType = MISSING,
         entity_metadata: Optional[GuildScheduledEventMetadata] = MISSING,
         status: GuildScheduledEventStatus = MISSING,
@@ -280,11 +341,14 @@ class GuildScheduledEvent(Hashable):
 
         Edits the guild scheduled event.
 
-        If updating ``entity_type`` to :class:`GuildScheduledEventEntityType.external`:
+        If updating ``entity_type`` to :attr:`GuildScheduledEventEntityType.external`:
 
-        - ``channel_id`` should be set to ``None`` or ignored
+        - ``channel`` should not be set
         - ``entity_metadata`` with a location field must be provided
         - ``scheduled_end_time`` must be provided
+
+        .. versionchanged:: 2.6
+            Removed ``channel_id`` parameter in favor of ``channel``.
 
         Parameters
         ----------
@@ -300,14 +364,17 @@ class GuildScheduledEvent(Hashable):
             .. versionchanged:: 2.5
                 Now accepts various resource types in addition to :class:`bytes`.
 
-        channel_id: Optional[:class:`int`]
-            The channel ID in which the guild scheduled event will be hosted.
+        channel: Optional[:class:`.abc.Snowflake`]
+            The channel in which the guild scheduled event will be hosted.
             Set to ``None`` if changing ``entity_type`` to :class:`GuildScheduledEventEntityType.external`.
+
+            .. versionadded:: 2.6
+
         privacy_level: :class:`GuildScheduledEventPrivacyLevel`
             The privacy level of the guild scheduled event.
         scheduled_start_time: :class:`datetime.datetime`
             The time to schedule the guild scheduled event.
-        scheduled_end_time: :class:`datetime.datetime`
+        scheduled_end_time: Optional[:class:`datetime.datetime`]
             The time when the guild scheduled event is scheduled to end.
         entity_type: :class:`GuildScheduledEventEntityType`
             The entity type of the guild scheduled event.
@@ -335,10 +402,6 @@ class GuildScheduledEvent(Hashable):
             The newly updated guild scheduled event instance.
         """
         fields: Dict[str, Any] = {}
-        is_external = entity_type is GuildScheduledEventEntityType.external
-        error_for_external_entity = (
-            "if entity_type is GuildScheduledEventEntityType.external, {} must be {}"
-        )
 
         if privacy_level is not MISSING:
             if not isinstance(privacy_level, GuildScheduledEventPrivacyLevel):
@@ -353,9 +416,6 @@ class GuildScheduledEvent(Hashable):
                 raise ValueError("entity_type must be an instance of GuildScheduledEventEntityType")
 
             fields["entity_type"] = entity_type.value
-
-        if not entity_metadata and is_external:
-            raise ValueError(error_for_external_entity.format("entity_metadata", "provided"))
 
         if entity_metadata is not MISSING:
             if entity_metadata is None:
@@ -384,22 +444,19 @@ class GuildScheduledEvent(Hashable):
         if image is not MISSING:
             fields["image"] = await _assetbytes_to_base64_data(image)
 
-        if channel_id is not MISSING:
-            if channel_id is not None and is_external:
-                raise ValueError(
-                    error_for_external_entity.format("channel_id", "None or not provided")
-                )
-            fields["channel_id"] = channel_id
-        elif channel_id is None and is_external:
+        if channel is not MISSING:
+            fields["channel_id"] = channel.id if channel is not None else None
+        elif entity_type is GuildScheduledEventEntityType.external:
+            # special case, as the API wants `channel_id=null` if type is being changed to `external`
             fields["channel_id"] = None
 
         if scheduled_start_time is not MISSING:
             fields["scheduled_start_time"] = scheduled_start_time.isoformat()
 
         if scheduled_end_time is not MISSING:
-            fields["scheduled_end_time"] = scheduled_end_time.isoformat()
-        elif is_external:
-            raise ValueError(error_for_external_entity.format("scheduled_end_time", "provided"))
+            fields["scheduled_end_time"] = (
+                scheduled_end_time.isoformat() if scheduled_end_time is not None else None
+            )
 
         data = await self._state.http.edit_guild_scheduled_event(
             guild_id=self.guild_id, event_id=self.id, reason=reason, **fields
