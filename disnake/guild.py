8a1c2184bf7e86e5a585f4aa8b758ead39dab5f7
shiftinv <me@shiftinv.cc>
2022-06-16 18:16:05 +0200

    docs: improve automod feature description

--- disnake/guild.py
+++ disnake/guild.py
@@ -206,7 +206,7 @@ class Guild(Hashable):
 
         - ``ANIMATED_BANNER``: Guild can upload an animated banner.
         - ``ANIMATED_ICON``: Guild can upload an animated icon.
-        - ``AUTO_MODERATION``: Guild has enabled auto moderation.
+        - ``AUTO_MODERATION``: Guild has set up auto moderation rules.
         - ``BANNER``: Guild can upload and use a banner. (i.e. :attr:`.banner`)
         - ``COMMUNITY``: Guild is a community server.
         - ``DISCOVERABLE``: Guild shows up in Server Discovery.
