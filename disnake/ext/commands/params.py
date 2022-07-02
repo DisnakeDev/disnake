7114e1eeffe76833bf9f2cc837eb22a5bc8c9ad9
shiftinv <8530778+shiftinv@users.noreply.github.com>
2022-07-02 17:07:29 +0200

    fix: correctly handle unions in `verify_type` (#584)

--- disnake/ext/commands/params.py
+++ disnake/ext/commands/params.py
@@ -36,6 +36,8 @@ from typing import (
     Callable,
     ClassVar,
     Dict,
+    Final,
+    FrozenSet,
     Generic,
     List,
     Literal,
@@ -44,7 +46,6 @@ from typing import (
     Type,
     TypeVar,
     Union,
-    cast,
     get_args,
     get_origin,
     get_type_hints,
@@ -116,8 +117,6 @@ def issubclass_(obj: Any, tp: Union[TypeT, Tuple[TypeT, ...]]) -> TypeGuard[Type
 def remove_optionals(annotation: Any) -> Any:
     """remove unwanted optionals from an annotation"""
     if get_origin(annotation) in (Union, UnionType):
-        annotation = cast(Any, annotation)
-
         args = tuple(i for i in annotation.__args__ if i not in (None, type(None)))
         if len(args) == 1:
             annotation = args[0]
@@ -280,6 +279,10 @@ class Range(type, metaclass=RangeMeta):
 
 class LargeInt(int):
     """Type for large integers in slash commands."""
+
+
+# option types that require additional handling in verify_type
+_VERIFY_TYPES: Final[FrozenSet[OptionType]] = frozenset((OptionType.user, OptionType.mentionable))
 
 
 class Private(Generic[T]):
@@ -449,14 +452,21 @@ class ParamInfo:
         return default
 
     async def verify_type(self, inter: ApplicationCommandInteraction, argument: Any) -> Any:
-        """Check if a type of an argument is correct and possibly fix it"""
-        if issubclass_(self.type, disnake.Member):
-            if isinstance(argument, disnake.Member):
-                return argument
+        """Check if the type of an argument is correct and possibly raise if it's not."""
+        if self.discord_type not in _VERIFY_TYPES:
+            return argument
 
+        # The API may return a `User` for options annotated with `Member`,
+        # including `Member` (user option), `Union[User, Member]` (user option) and
+        # `Union[Member, Role]` (mentionable option).
+        # If we received a `User` but didn't expect one, raise.
+        if (
+            isinstance(argument, disnake.User)
+            and issubclass_(self.type, disnake.Member)
+            and not issubclass_(self.type, disnake.User)
+        ):
             raise errors.MemberNotFound(str(argument.id))
 
-        # unexpected types may just be ignored
         return argument
 
     async def convert_argument(self, inter: ApplicationCommandInteraction, argument: Any) -> Any:
