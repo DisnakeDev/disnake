25943c296514cc4bf1db216b018007e553804987
shiftinv <me@shiftinv.cc>
2022-07-16 16:51:54 +0200

    fix: flake8 errors

--- tests/ui/test_decorators.py
+++ tests/ui/test_decorators.py
@@ -13,7 +13,7 @@ T = TypeVar("T", bound=ui.Item)
 @contextlib.contextmanager
 def create_callback(item_type: Type[T]) -> Iterator["ui.item.ItemCallbackType[T]"]:
     async def callback(self, item, inter):
-        assert False
+        pytest.fail("callback should not be invoked")
 
     yield callback
 
@@ -45,7 +45,7 @@ class TestDecorator:
     # from here on out we're only testing the button decorator,
     # as @ui.select works identically
 
-    @pytest.mark.parametrize("cls", (_CustomButton, _CustomButton[Any]))
+    @pytest.mark.parametrize("cls", [_CustomButton, _CustomButton[Any]])
     def test_cls(self, cls: Type[_CustomButton]) -> None:
         with create_callback(cls) as func:
             res = ui.button(cls=cls, param=1337)(func)
@@ -62,7 +62,7 @@ class TestDecorator:
             this_should_not_work="h",  # type: ignore
         )
 
-    @pytest.mark.parametrize("cls", (123, int, ui.Select))
+    @pytest.mark.parametrize("cls", [123, int, ui.Select])
     def test_cls_invalid(self, cls) -> None:
         with pytest.raises(TypeError, match=r"cls argument must be"):
             ui.button(cls=cls)  # type: ignore
