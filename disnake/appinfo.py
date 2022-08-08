6f9d573cd9e80aa43bbbecffce786d57cd5ada02
shiftinv <me@shiftinv.cc>
2022-08-08 23:38:41 +0200

    refactor: make `AppInfo` subclass of `PartialAppInfo`

--- disnake/appinfo.py
+++ disnake/appinfo.py
@@ -89,7 +89,7 @@ class InstallParams:
 
 
 class PartialAppInfo:
-    """Represents a partial AppInfo given by :func:`~disnake.abc.GuildChannel.create_invite`.
+    """Represents partial application information, for example applications in invites.
 
     .. versionadded:: 2.0
 
@@ -101,7 +101,7 @@ class PartialAppInfo:
         The application's name.
     description: :class:`str`
         The application's description.
-    rpc_origins: Optional[List[:class:`str`]]
+    rpc_origins: List[:class:`str`]
         A list of RPC origin URLs, if RPC is enabled.
     verify_key: :class:`str`
         The hex encoded key for verification in interactions and the
@@ -131,7 +131,7 @@ class PartialAppInfo:
         self.name: str = data["name"]
         self._icon: Optional[str] = data.get("icon")
         self.description: str = data["description"]
-        self.rpc_origins: Optional[List[str]] = data.get("rpc_origins")
+        self.rpc_origins: List[str] = data.get("rpc_origins") or []
         self._summary: str = data.get("summary", "")
         self.verify_key: str = data["verify_key"]
         self.terms_of_service_url: Optional[str] = data.get("terms_of_service_url")
@@ -163,15 +163,11 @@ class PartialAppInfo:
         return self._summary
 
 
-class AppInfo:
+class AppInfo(PartialAppInfo):
     """Represents the application info for the bot provided by Discord.
 
     Attributes
     ----------
-    id: :class:`int`
-        The application's ID.
-    name: :class:`str`
-        The application's name.
     owner: :class:`User`
         The application's owner.
     team: Optional[:class:`Team`]
@@ -179,22 +175,12 @@ class AppInfo:
 
         .. versionadded:: 1.3
 
-    description: :class:`str`
-        The application's description.
     bot_public: :class:`bool`
         Whether the bot can be invited by anyone or if it is locked
         to the application owner.
     bot_require_code_grant: :class:`bool`
         Whether the bot requires the completion of the full oauth2 code
         grant flow to join.
-    rpc_origins: Optional[List[:class:`str`]]
-        A list of RPC origin URLs, if RPC is enabled.
-    verify_key: :class:`str`
-        The hex encoded key for verification in interactions and the
-        GameSDK's `GetTicket <https://discord.com/developers/docs/game-sdk/applications#getticket>`_.
-
-        .. versionadded:: 1.3
-
     guild_id: Optional[:class:`int`]
         If this application is a game sold on Discord,
         this field will be the guild to which it has been linked to.
@@ -213,16 +199,6 @@ class AppInfo:
         this field will be the URL slug that links to the store page.
 
         .. versionadded:: 1.3
-
-    terms_of_service_url: Optional[:class:`str`]
-        The application's terms of service URL, if set.
-
-        .. versionadded:: 2.0
-
-    privacy_policy_url: Optional[:class:`str`]
-        The application's privacy policy URL, if set.
-
-        .. versionadded:: 2.0
 
     flags: Optional[:class:`ApplicationFlags`]
         The application's public flags.
@@ -246,24 +222,14 @@ class AppInfo:
     """
 
     __slots__ = (
-        "_state",
-        "description",
-        "id",
-        "name",
-        "rpc_origins",
         "bot_public",
         "bot_require_code_grant",
         "owner",
-        "_icon",
-        "_summary",
-        "verify_key",
         "team",
         "guild_id",
         "primary_sku_id",
         "slug",
         "_cover_image",
-        "terms_of_service_url",
-        "privacy_policy_url",
         "flags",
         "tags",
         "install_params",
@@ -271,14 +237,10 @@ class AppInfo:
     )
 
     def __init__(self, state: ConnectionState, data: AppInfoPayload):
+        super().__init__(state=state, data=data)
+
         from .team import Team
 
-        self._state: ConnectionState = state
-        self.id: int = int(data["id"])
-        self.name: str = data["name"]
-        self.description: str = data["description"]
-        self._icon: Optional[str] = data["icon"]
-        self.rpc_origins: List[str] = data["rpc_origins"]
         self.bot_public: bool = data["bot_public"]
         self.bot_require_code_grant: bool = data["bot_require_code_grant"]
         self.owner: User = state.create_user(data["owner"])
@@ -286,16 +248,11 @@ class AppInfo:
         team: Optional[TeamPayload] = data.get("team")
         self.team: Optional[Team] = Team(state, team) if team else None
 
-        self._summary: str = data.get("summary", "")
-        self.verify_key: str = data["verify_key"]
-
         self.guild_id: Optional[int] = utils._get_as_snowflake(data, "guild_id")
 
         self.primary_sku_id: Optional[int] = utils._get_as_snowflake(data, "primary_sku_id")
         self.slug: Optional[str] = data.get("slug")
         self._cover_image: Optional[str] = data.get("cover_image")
-        self.terms_of_service_url: Optional[str] = data.get("terms_of_service_url")
-        self.privacy_policy_url: Optional[str] = data.get("privacy_policy_url")
 
         flags: Optional[int] = data.get("flags")
         self.flags: Optional[ApplicationFlags] = (
@@ -315,13 +272,6 @@ class AppInfo:
         )
 
     @property
-    def icon(self) -> Optional[Asset]:
-        """Optional[:class:`.Asset`]: Retrieves the application's icon asset, if any."""
-        if self._icon is None:
-            return None
-        return Asset._from_icon(self._state, self.id, self._icon, path="app")
-
-    @property
     def cover_image(self) -> Optional[Asset]:
         """Optional[:class:`.Asset`]: Retrieves the cover image on a store embed, if any.
 
@@ -339,20 +289,3 @@ class AppInfo:
         .. versionadded:: 1.3
         """
         return self._state._get_guild(self.guild_id)
-
-    @property
-    def summary(self) -> str:
-        """:class:`str`: If this application is a game sold on Discord,
-        this field will be the summary field for the store page of its primary SKU.
-
-        .. versionadded:: 1.3
-
-        .. deprecated:: 2.5
-
-            This field is deprecated by discord and is now always blank. Consider using :attr:`.description` instead.
-        """
-        utils.warn_deprecated(
-            "summary is deprecated and will be removed in a future version. Consider using description instead.",
-            stacklevel=2,
-        )
-        return self._summary
--- disnake/types/appinfo.py
+++ disnake/types/appinfo.py
@@ -41,6 +41,12 @@ class BaseAppInfo(TypedDict):
     description: str
 
 
+class PartialAppInfo(BaseAppInfo, total=False):
+    rpc_origins: List[str]
+    terms_of_service_url: str
+    privacy_policy_url: str
+
+
 class InstallParams(TypedDict):
     scopes: List[str]
     permissions: str
@@ -51,34 +57,21 @@ class _AppInfoOptional(TypedDict, total=False):
     guild_id: Snowflake
     primary_sku_id: Snowflake
     slug: str
-    terms_of_service_url: str
-    privacy_policy_url: str
     hook: bool
     max_participants: int
     tags: List[str]
     install_params: InstallParams
     custom_install_url: str
-
-
-class AppInfo(BaseAppInfo, _AppInfoOptional):
-    rpc_origins: List[str]
-    owner: User
-    bot_public: bool
-    bot_require_code_grant: bool
-
-
-class _PartialAppInfoOptional(TypedDict, total=False):
-    rpc_origins: List[str]
     cover_image: str
     hook: bool
-    terms_of_service_url: str
-    privacy_policy_url: str
     max_participants: int
     flags: int
 
 
-class PartialAppInfo(_PartialAppInfoOptional, BaseAppInfo):
-    pass
+class AppInfo(PartialAppInfo, _AppInfoOptional):
+    owner: User
+    bot_public: bool
+    bot_require_code_grant: bool
 
 
 class PartialGatewayAppInfo(TypedDict):
