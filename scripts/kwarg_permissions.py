c36ce95a33f426422baa3d37ace493d4b12b3bde
onerandomusername <genericusername414+git@gmail.com>
2022-08-16 19:29:17 -0400

    chore: refactor kwarg_permissions codemod

--- scripts/kwarg_permissions.py
+++ scripts/kwarg_permissions.py
@@ -24,13 +24,15 @@ class PermissionTypings(codemod.VisitorBasedCodemodCommand):
         return super().transform_module(tree)
 
     def visit_FunctionDef(self, node: cst.FunctionDef) -> Optional[bool]:
+        # don't recurse into the body of a function
         return False
 
-    def leave_FunctionDef(self, node: cst.FunctionDef, new_node: cst.FunctionDef):
+    def leave_FunctionDef(self, _: cst.FunctionDef, node: cst.FunctionDef):
+        # we don't care about with the unedited note
         has_overload_deco = False
         is_overload = False
         previously_generated = False
-        for deco in new_node.decorators:
+        for deco in node.decorators:
             if isinstance(deco.decorator, cst.Call):
                 continue
             name = deco.decorator.value
@@ -45,15 +47,13 @@ class PermissionTypings(codemod.VisitorBasedCodemodCommand):
             return cst.RemovalSentinel.REMOVE
 
         if not has_overload_deco:
-            return new_node
+            return node
 
-        node = new_node
-
-        # if the function is not an overload and is decorated with the deco
         if not node.params.star_kwarg and not is_overload:
             raise RuntimeError(
-                'a function cannot be decorated with "_overload_with_permissions" and not take any kwargs.'
+                'a function cannot be decorated with "_overload_with_permissions" and not take any kwargs unless it is an overload.'
             )
+        # always true if this isn't an overload
         elif node.params.star_kwarg:
             # use the existing annotation if one exists
             annotation = node.params.star_kwarg.annotation
@@ -61,6 +61,7 @@ class PermissionTypings(codemod.VisitorBasedCodemodCommand):
                 raise RuntimeError(
                     f"parameter {node.params.star_kwarg.name.value} must be annotated."
                 )
+        # only possible in the case of an overload
         else:
             # use the first permission annotation if it exists, otherwise default to `bool`
             # make a matcher instance of all valid permission names
@@ -98,14 +99,32 @@ class PermissionTypings(codemod.VisitorBasedCodemodCommand):
                 star_arg=star_arg,
             )
 
-        # now we have the function, we can add all of the permissions
+        def get_perm_kwargs():
+            return [
+                cst.Param(
+                    cst.Name(perm),
+                    annotation,
+                    default=cst.Ellipsis(),
+                )
+                for perm in self.permissions
+            ]
+
+        # get a Params with all of the new params that we should have
         params = remove_existing_permissions(node.params)
-        # also remove the permissions from the original node
-        node = node.with_changes(params=params)
-        # remove kwargs from the params
         params = params.with_changes(star_kwarg=None)
+        empty_overload_params = params.deep_clone()
+
+        # add the permissions to the kw_only params
+        kwonly_params = list(params.kwonly_params)
+        kwonly_params.extend(get_perm_kwargs())
+        params = params.with_changes(kwonly_params=kwonly_params)
+
+        if is_overload:
+            node = node.with_changes(params=params)
+            return node
+
         # make an overload before permissions
-        empty_overload = node.deep_clone().with_changes(params=params)
+        empty_overload = node.deep_clone().with_changes(params=empty_overload_params)
         empty_overload = empty_overload.with_changes(
             body=cst.IndentedBlock([cst.SimpleStatementLine([cst.Expr(cst.Ellipsis())])]),
             decorators=[
@@ -113,33 +132,12 @@ class PermissionTypings(codemod.VisitorBasedCodemodCommand):
                 cst.Decorator(cst.Name("_generated")),
             ],
         )
-
-        perms = []
-        # we use the annotation of kwargs
-
-        for perm in self.permissions:
-            perms.append(
-                cst.Param(
-                    cst.Name(perm),
-                    annotation,
-                    default=cst.Ellipsis(),
-                )
-            )
-
-        codevisitors.AddImportsVisitor.add_needed_import(self.context, "typing", "overload")
+        # if the decorated method is an overload we make an in-place change and don't add overloads
 
         overload = empty_overload.deep_clone()
-        params = remove_existing_permissions(overload.params)
-        kwonly_params = list(params.kwonly_params)
-        kwonly_params.extend(perms)
-        params = params.with_changes(kwonly_params=kwonly_params, star_kwarg=None)
-        if is_overload:
-            node = node.with_changes(params=params)
-            return node
-
         overload = overload.with_changes(params=params)
 
-        # only add the import if we're using the empty overload
+        codevisitors.AddImportsVisitor.add_needed_import(self.context, "typing", "overload")
         codevisitors.AddImportsVisitor.add_needed_import(
             self.context, "disnake.utils", "_generated"
         )
