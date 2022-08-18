6712f1be4f0e6a9a110fb3c2bf829b8636ea4638
onerandomusername <genericusername414+git@gmail.com>
2022-08-18 00:32:33 -0400

    refactor MISSING overloads

--- scripts/codemods/overloads_no_missing.py
+++ scripts/codemods/overloads_no_missing.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Sequence
 
 import libcst as cst
 import libcst.matchers as m
@@ -22,28 +22,21 @@ class EllipsisOverloads(codemod.VisitorBasedCodemodCommand):
         else:
             return node
 
+        kw = {}
         for param_type in ["params", "kwonly_params", "posonly_params"]:
-            params: List[cst.Param] = getattr(node.params, param_type, [])
+            params: Sequence[cst.Param] = getattr(node.params, param_type)
             if not params:
                 continue
 
             new_params: List[cst.Param] = []
             for param in params:
-                if not param.default:
-                    new_params.append(param)
-                    continue
-                if isinstance(param.default, cst.Ellipsis):
-                    new_params.append(param)
-                    continue
-                if not isinstance(param.default, cst.Name):
-                    new_params.append(param)
-                elif param.default.value == "MISSING":
+                if param.default and m.matches(param.default, m.Name("MISSING")):
                     new_param = param.with_changes(default=cst.Ellipsis())
                     new_params.append(new_param)
                 else:
                     new_params.append(param)
-            kw = {}
             kw[param_type] = new_params
-            node = node.with_deep_changes(node.params, **kw)
+
+        node = node.with_deep_changes(node.params, **kw)
 
         return node
