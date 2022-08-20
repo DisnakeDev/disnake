9a2d89b3981dbf2f01c00b88051526871d262f46
shiftinv <me@shiftinv.cc>
2022-08-20 13:15:09 +0200

    fix: require `SphinxExtensionMeta` to not be None
    we'll want to at least set `parallel_read_safe=True` in 99% of cases, so this should help avoid skipping that

--- docs/extensions/_types.py
+++ docs/extensions/_types.py
@@ -1,11 +1,8 @@
-from typing import Optional, TypedDict
+from typing import TypedDict
 
 
-class _SphinxExtensionMeta(TypedDict, total=False):
+class SphinxExtensionMeta(TypedDict, total=False):
     version: str
     env_version: int
     parallel_read_safe: bool
     parallel_write_safe: bool
-
-
-SphinxExtensionMeta = Optional[_SphinxExtensionMeta]
