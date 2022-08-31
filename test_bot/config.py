df9d4203a1c3acbd95aed188ee0ecc28f3523938
onerandomusername <genericusername414+git@gmail.com>
2022-08-31 12:57:14 -0400

    chore: use a util for getting booleans

--- test_bot/config.py
+++ test_bot/config.py
@@ -9,18 +9,22 @@ else:
     dotenv.load_dotenv()
 
 
+def get_bool(env_var: str, *, default: bool = False) -> bool:
+    return os.environ.get(env_var, "true" if default else "false").lower() == "true"
+
+
 class Config:
     token = os.environ.get("BOT_TOKEN")
     prefix = os.environ.get("PREFIX", ".")
     cogs_folder = os.environ.get("COGS_FOLDER", "cogs")
-    auto_reload = os.environ.get("AUTO_RELOAD", "false") == "true"
-    strict_localization = os.environ.get("STRICT_LOCALIZATION", "true") == "true"
-    sync_commands_debug = os.environ.get("SYNC_COMMANDS_DEBUG", "true") == "true"
+    auto_reload = get_bool("AUTO_RELOAD")
+    strict_localization = get_bool("STRICT_LOCALIZATION", default=True)
+    sync_commands_debug = get_bool("SYNC_COMMANDS_DEBUG", default=True)
     test_guilds = (
         [int(x.strip()) for x in test_guilds.split(",")]
         if (test_guilds := os.environ.get("TEST_GUILDS", None))
         else None
     )
-    debug = os.environ.get("DEBUG", "false") == "true"
+    debug = get_bool("DEBUG")
     debug_loggers = os.environ.get("DEBUG_LOGGERS")
-    log_to_file = os.environ.get("LOG_TO_FILE", "false").lower() == "true"
+    log_to_file = get_bool("LOG_TO_FILE")
