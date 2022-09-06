e3f3f1059f766f5c3ac2056eea271b524b8abfe7
Snipy7374 <davidecucci07@gmail.com>
2022-09-06 11:03:15 +0200

    Update disnake/message.py
    Co-authored-by: shiftinv <8530778+shiftinv@users.noreply.github.com>

--- disnake/message.py
+++ disnake/message.py
@@ -752,7 +752,7 @@ class Message(Hashable):
     position: Optional[:class:`int`]
         A number that indicate the approximate position of a message in a :class:`Thread`.
         This is a number that starts at 0. e.g. the first message is position 0.
-        Is `None` if the message that was sent is not from a :class:`Thread`.
+        This is `None` if the message that was sent is not from a :class:`Thread`.
 
         .. versionadded:: 2.6
 
