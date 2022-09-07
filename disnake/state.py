751dfa9d9e3de9ce9da19452c0d4802b18604bde
Snipy7374 <davidecucci07@gmail.com>
2022-09-07 11:10:04 +0200

    Update state.py

--- disnake/state.py
+++ disnake/state.py
@@ -786,7 +786,7 @@ class ConnectionState:
         if channel:
             if channel.__class__ in (TextChannel, Thread, VoiceChannel):
                 channel.last_message_id = message.id  # type: ignore
-            if type(channel) is Thread and message.type is not MessageType.thread_starter_message:
+            if channel.__class__ is Thread and message.type is not MessageType.thread_starter_message:
                 channel.total_message_sent += 1
                 channel.message_count += 1
 
