e1ff6ed64c0b2af783ba202f196061460286865b
ItsAleph <igorekkrupskij@gmail.com>
2022-09-09 14:44:30 +0300

    thanks read the docs

--- docs/_static/scroll_sidebar.js
+++ docs/_static/scroll_sidebar.js
@@ -2,11 +2,23 @@
 
 window.addEventListener("DOMContentLoaded", main);
 
+const url = new URL(window.location.href);
+
 function main() {
+    let num = 0;
+
+    if (url.host.startsWith("disnake--") && url.host.endsWith(".org.readthedocs.build")) {
+        // when building docs for PRs, rtd inserts the fucking
+        // "warning" telling that the page was created from PR
+        // just before the main ul, thus we need to increment
+        // this to skip admonition and get ul instead of div
+        num = 1;
+    }
+
     const main_ul = document.getElementById('sidebar')
         .children[0]
         .children[1]
-        .children[0];
+        .children[num];
 
     for (const elem of main_ul.children) {
         if (elem.children[1].href === undefined) { // pls dont even ask how this works
