f6861fb7ab6f49d2871579b2eb66aa6c19b5387b
ItsAleph <igorekkrupskij@gmail.com>
2022-09-11 01:34:40 +0300

    ItsNotMyFault:tm:

--- docs/_static/scroll_sidebar.js
+++ docs/_static/scroll_sidebar.js
@@ -5,15 +5,10 @@ window.addEventListener("DOMContentLoaded", main);
 const url_ss = new URL(window.location.href);
 
 function main() {
-    const main_div = document.getElementById('sidebar')
+    const main_ul = document.getElementById('sidebar')
         .children[0]
-        .children[1];
-
-    if (main_div.tagName === 'DIV') {
-        main_div.removeChild(main_div.children[0]); // remove admonition from sidebar
-    }
-
-    const main_ul = main_div.children[0];
+        .children[1]
+        .children[0];
 
     for (const elem of main_ul.children) {
         if (elem.children[1].href === undefined) { // pls dont even ask how this works, its javascript
