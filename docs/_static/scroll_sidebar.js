2aacbeb9708b3843d27ab8bf86e7ff5b5c39d9ef
ItsAleph <igorekkrupskij@gmail.com>
2022-09-10 18:25:10 +0300

    gg

--- docs/_static/scroll_sidebar.js
+++ docs/_static/scroll_sidebar.js
@@ -5,23 +5,13 @@ window.addEventListener("DOMContentLoaded", main);
 const url_ss = new URL(window.location.href);
 
 function main() {
-    let num = 0;
-
-    if (url_ss.host.startsWith("disnake--") && url_ss.host.endsWith(".org.readthedocs.build")) {
-        // when building docs for PRs, rtd inserts the fucking
-        // "warning" telling that the page was created from PR
-        // just before the main ul, thus we need to increment
-        // this to skip admonition and get ul instead of div
-        num = 1;
-    }
-
     const main_ul = document.getElementById('sidebar')
         .children[0]
         .children[1]
-        .children[num];
+        .children[0];
 
     for (const elem of main_ul.children) {
-        if (elem.children[1].href === undefined) { // pls dont even ask how this works
+        if (elem.children[1].href === undefined) { // pls dont even ask how this works, its javascript
             elem.scrollIntoView();
             break;
         }
--- docs/extensions/ignore_rtd_banner.py
+++ docs/extensions/ignore_rtd_banner.py
@@ -4,13 +4,7 @@ from sphinx.application import Sphinx
 def main(app: Sphinx) -> None:
     for index, listener in enumerate(app.events.listeners.get("doctree-resolved")):  # type: ignore
         if listener.handler.__qualname__ == "process_external_version_warning_banner":
-            print("OMFG IT WORKS")
-            print("BEFORE: %s" % app.events.listeners.get("doctree-resolved"))
-            print()
             app.events.listeners["doctree-resolved"].pop(index)
-            print("AFTER: %s" % app.events.listeners.get("doctree-resolved"))
-        else:
-            print("FUCK: %s" % listener.handler.__qualname__)
 
 
 def setup(app: Sphinx) -> None:
