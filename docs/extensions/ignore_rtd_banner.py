c484892588c071395fc45216393f4035f7d23571
ItsAleph <igorekkrupskij@gmail.com>
2022-09-10 21:13:15 +0300

    test

--- docs/extensions/ignore_rtd_banner.py
+++ docs/extensions/ignore_rtd_banner.py
@@ -2,6 +2,7 @@ from sphinx.application import Sphinx
 
 
 def main(app: Sphinx) -> None:
+    return
     for index, listener in enumerate(app.events.listeners.get("doctree-resolved")):  # type: ignore
         if listener.handler.__qualname__ == "process_external_version_warning_banner":
             app.events.listeners["doctree-resolved"].pop(index)
