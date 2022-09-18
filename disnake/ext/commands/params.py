54050aaa445406c1db25a837c05704260ab71746
weerdy15 <77104725+ItsAleph@users.noreply.github.com>
2022-09-18 10:06:55 +0300

    Update disnake/ext/commands/params.py
    Co-authored-by: shiftinv <8530778+shiftinv@users.noreply.github.com>

--- disnake/ext/commands/params.py
+++ disnake/ext/commands/params.py
@@ -1303,12 +1303,6 @@ def register_injection(
     -------
     :class:`Injection`
         The injection being registered.
-
-        .. note::
-
-            In terms of typing, this returns ``Any`` to avoid typing issues caused by how this
-            extension work, but at runtime this is always an :class:`Injection` instance.
-            You can find more in-depth explanation :ref:`here <why_params_and_injections_return_any>`.
     """
     sig = signature(function)
     tp = sig.return_annotation
