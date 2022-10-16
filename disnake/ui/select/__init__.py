9357233e48f5f7cf2e1dbb0874bdb54464913482
shiftinv <me@shiftinv.cc>
2022-10-16 18:23:54 +0200

    fix: add `disnake.ui.select.__all__`

--- disnake/ui/select/__init__.py
+++ disnake/ui/select/__init__.py
@@ -10,5 +10,10 @@ Select Menu UI Kit Types
 :license: MIT, see LICENSE for more details.
 
 """
+from . import base, string
 from .base import *
 from .string import *
+
+__all__ = []
+__all__.extend(base.__all__)
+__all__.extend(string.__all__)
