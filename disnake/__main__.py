b37240ff43736077a30b7451024463ee1af8b8d3
arl <me@arielle.codes>
2022-10-22 09:32:15 -0400

    feat(cli): improve the experience by providing help by default and allowing InteractionBot to be used (#791)
    Co-authored-by: shiftinv <8530778+shiftinv@users.noreply.github.com>

--- disnake/__main__.py
+++ disnake/__main__.py
@@ -1,12 +1,12 @@
 # SPDX-License-Identifier: MIT
 
 import argparse
+import importlib.metadata
 import platform
 import sys
 from pathlib import Path
 
 import aiohttp
-import pkg_resources
 
 import disnake
 
@@ -22,29 +22,42 @@ def show_version():
     entries.append(
         f"- disnake v{disnake_ver.major}.{disnake_ver.minor}.{disnake_ver.micro}-{disnake_ver.releaselevel}"
     )
-    if pkg := pkg_resources.get_distribution("disnake"):
-        entries.append(f"    - disnake pkg_resources: v{pkg.version}")
+    try:
+        version = importlib.metadata.version("disnake")
+    except importlib.metadata.PackageNotFoundError:
+        pass
+    else:
+        entries.append(f"    - disnake importlib.metadata: v{version}")
 
     entries.append(f"- aiohttp v{aiohttp.__version__}")
     uname = platform.uname()
-    entries.append(f"- system info: {uname.system} {uname.release} {uname.version}")
+    entries.append(f"- system info: {uname.system} {uname.release} {uname.version} {uname.machine}")
     print("\n".join(entries))
 
 
-def core(parser, args):
+def core(parser: argparse.ArgumentParser, args):
+    # this method runs when no subcommands are provided
+    # as such, we can assume that we want to print help
     if args.version:
         show_version()
+    else:
+        parser.print_help()
 
 
+_interaction_bot_init = """super().__init__(**kwargs)"""
+_commands_bot_init = (
+    'super().__init__(command_prefix=commands.when_mentioned_or("{prefix}"), **kwargs)'
+)
 _bot_template = """#!/usr/bin/env python3
 
 from disnake.ext import commands
 import disnake
 import config
 
+
 class Bot(commands.{base}):
     def __init__(self, **kwargs):
-        super().__init__(command_prefix=commands.when_mentioned_or("{prefix}"), **kwargs)
+        {init}
         for cog in config.cogs:
             try:
                 self.load_extension(cog)
@@ -59,7 +72,8 @@ bot = Bot()
 
 # write general commands here
 
-bot.run(config.token)
+if __name__ == "__main__":
+    bot.run(config.token)
 """
 
 _gitignore_template = """# Byte-compiled / optimized / DLL files
@@ -214,8 +228,13 @@ def newbot(parser, args):
 
     try:
         with open(str(new_directory / "bot.py"), "w", encoding="utf-8") as fp:
-            base = "Bot" if not args.sharded else "AutoShardedBot"
-            fp.write(_bot_template.format(base=base, prefix=args.prefix))
+            if args.interaction_client:
+                init = _interaction_bot_init
+                base = "AutoShardedInteractionBot" if args.sharded else "InteractionBot"
+            else:
+                init = _commands_bot_init.format(prefix=args.prefix)
+                base = "AutoShardedBot" if args.sharded else "Bot"
+            fp.write(_bot_template.format(base=base, init=init))
     except OSError as exc:
         parser.error(f"could not create bot file ({exc})")
 
@@ -271,10 +290,20 @@ def add_newbot_args(subparser):
     parser.add_argument(
         "directory", help="the directory to place it in (default: .)", nargs="?", default=Path.cwd()
     )
-    parser.add_argument(
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
         "--prefix", help="the bot prefix (default: $)", default="$", metavar="<prefix>"
     )
-    parser.add_argument("--sharded", help="whether to use AutoShardedBot", action="store_true")
+    group.add_argument(
+        "--app-commands-only",
+        help="whether to only process application commands",
+        action="store_true",
+        dest="interaction_client",
+    )
+    parser.add_argument(
+        "--sharded", help="whether to use an automatically sharded bot", action="store_true"
+    )
     parser.add_argument(
         "--no-git", help="do not create a .gitignore file", action="store_true", dest="no_git"
     )
