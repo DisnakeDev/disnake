7ef0ddbdfc4820604d75463b37c3187275f19021
shiftinv <me@shiftinv.cc>
2023-01-10 00:21:26 +0100

    fix: `StickerPack.cover_sticker_id` and `.banner_asset_id` are optional

--- disnake/sticker.py
+++ disnake/sticker.py
@@ -9,7 +9,7 @@ from .asset import Asset, AssetMixin
 from .enums import StickerFormatType, StickerType, try_enum
 from .errors import InvalidData
 from .mixins import Hashable
-from .utils import MISSING, cached_slot_property, find, get, snowflake_time
+from .utils import MISSING, _get_as_snowflake, cached_slot_property, find, get, snowflake_time
 
 __all__ = (
     "StickerPack",
@@ -67,9 +67,9 @@ class StickerPack(Hashable):
         The stickers of this sticker pack.
     sku_id: :class:`int`
         The SKU ID of the sticker pack.
-    cover_sticker_id: :class:`int`
+    cover_sticker_id: Optional[:class:`int`]
          The ID of the sticker used for the cover of the sticker pack.
-    cover_sticker: :class:`StandardSticker`
+    cover_sticker: Optional[:class:`StandardSticker`]
         The sticker used for the cover of the sticker pack.
     """
 
@@ -97,14 +97,16 @@ class StickerPack(Hashable):
         ]
         self.name: str = data["name"]
         self.sku_id: int = int(data["sku_id"])
-        self.cover_sticker_id: int = int(data["cover_sticker_id"])
-        self.cover_sticker: StandardSticker = get(self.stickers, id=self.cover_sticker_id)  # type: ignore
+        self.cover_sticker_id: Optional[int] = _get_as_snowflake(data, "cover_sticker_id")
+        self.cover_sticker: Optional[StandardSticker] = get(self.stickers, id=self.cover_sticker_id)
         self.description: str = data["description"]
-        self._banner: int = int(data["banner_asset_id"])
+        self._banner: Optional[int] = _get_as_snowflake(data, "banner_asset_id")
 
     @property
-    def banner(self) -> Asset:
-        """:class:`Asset`: The banner asset of the sticker pack."""
+    def banner(self) -> Optional[Asset]:
+        """Optional[:class:`Asset`]: The banner asset of the sticker pack."""
+        if not self._banner:
+            return None
         return Asset._from_sticker_banner(self._state, self._banner)
 
     def __repr__(self) -> str:
