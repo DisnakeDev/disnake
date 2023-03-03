0ce581cc491172e35e35d374a3518fb9ea10d62e
EQUENOS <akkaunt.vzapase@gmail.com>
2023-03-04 01:13:13 +0300

    Prevent unnecessary `collect_params` calls
    Also fixed `LocalizationValue._copy`

--- disnake/ext/commands/params.py
+++ disnake/ext/commands/params.py
@@ -929,6 +929,10 @@ def collect_params(
 
     Returns: (`cog parameter`, `interaction parameter`, `param infos`, `injections`)
     """
+    # any information we collect here does not change over time so we can cache it
+    if hasattr(function, "__collect_params_cache__"):
+        return function.__collect_params_cache__
+
     (cog_param, inter_param), parameters = isolate_self(function)
     doc = disnake.utils.parse_docstring(function)["params"]
 
@@ -964,18 +968,18 @@ def collect_params(
             paraminfo = ParamInfo.from_param(parameter, {}, doc)
             paraminfos.append(paraminfo)
 
-    return (
+    function.__collect_params_cache__ = (
         cog_param.name if cog_param else None,
         inter_param.name if inter_param else None,
         paraminfos,
         injections,
     )
+    return function.__collect_params_cache__
 
 
 def collect_nested_params(function: Callable) -> List[ParamInfo]:
     """Collect all options from a function"""
     # TODO: Have these be actually sorted properly and not have injections always at the end
-
     _, _, paraminfos, injections = collect_params(function)
 
     for injection in injections.values():
@@ -1032,6 +1036,8 @@ async def call_param_func(
     function: Callable, interaction: ApplicationCommandInteraction, /, *args: Any, **kwargs: Any
 ) -> Any:
     """Call a function utilizing ParamInfo"""
+    # Unlike collect_params, this function depends on interactions because it handles conversion,
+    # therefore the result of this function can not be cached and reused.
     cog_param, inter_param, paraminfos, injections = collect_params(function)
     formatted_kwargs = format_kwargs(interaction, cog_param, inter_param, *args, **kwargs)
     formatted_kwargs.update(await run_injections(injections, interaction, *args, **kwargs))
