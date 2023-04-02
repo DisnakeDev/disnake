d9d0c00772c29d379e72a40b9711581a36588b8b
Victorsitou <67214928+Victorsitou@users.noreply.github.com>
2023-04-01 23:12:47 -0300

    typing: add type annotations

--- disnake/onboarding.py
+++ disnake/onboarding.py
@@ -1,12 +1,13 @@
 # SPDX-License-Identifier: MIT
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING, FrozenSet, List, Optional, Union
 
 from .enums import OnboardingPromptType, try_enum
 from .mixins import Hashable
 
 if TYPE_CHECKING:
+    from .emoji import Emoji, PartialEmoji
     from .guild import Guild, GuildChannel
     from .role import Role
     from .types.onboarding import (
@@ -47,15 +48,15 @@ class Onboarding:
     )
 
     def __init__(self, *, guild: Guild, data: OnboardingPayload):
-        self.guild = guild
+        self.guild: Guild = guild
         self._from_data(data)
 
     def _from_data(self, data: OnboardingPayload):
-        self.prompts = [
+        self.prompts: List[OnboardingPrompt] = [
             OnboardingPrompt(data=prompt, guild=self.guild) for prompt in data["prompts"]
         ]
-        self.enabled = data["enabled"]
-        self.default_channel_ids = (
+        self.enabled: bool = data["enabled"]
+        self.default_channel_ids: FrozenSet[int] = (
             frozenset(map(int, exempt_channels))
             if (exempt_channels := data["default_channel_ids"])
             else frozenset()
@@ -110,15 +111,15 @@ class OnboardingPrompt(Hashable):
     def __init__(self, *, guild: Guild, data: OnboardingPromptPayload):
         self.guild = guild
 
-        self.id = int(data["id"])
-        self.title = data["title"]
-        self.options = [
+        self.id: int = int(data["id"])
+        self.title: str = data["title"]
+        self.options: List[OnboardingPromptOption] = [
             OnboardingPromptOption(data=option, guild=guild) for option in data["options"]
         ]
-        self.single_select = data["single_select"]
-        self.required = data["required"]
-        self.in_onboarding = data["in_onboarding"]
-        self.type = try_enum(OnboardingPromptType, data["type"])
+        self.single_select: bool = data["single_select"]
+        self.required: bool = data["required"]
+        self.in_onboarding: bool = data["in_onboarding"]
+        self.type: OnboardingPromptType = try_enum(OnboardingPromptType, data["type"])
 
     def __str__(self) -> str:
         return self.title
@@ -160,20 +161,21 @@ class OnboardingPromptOption(Hashable):
         # so the API needs the timestamp to know what ID it needs to create, should we just add a note about it
         # or "try" to create the ID ourselves?
         # I'm not sure if this also happens for OnboardingPrompt
-        self.guild = guild
+        self.guild: Guild = guild
 
-        self.id = int(data["id"])
-        self.title = data["title"]
-        self.description = data["description"]
-        self.roles_ids = (
+        self.id: int = int(data["id"])
+        self.title: str = data["title"]
+        self.description: Optional[str] = data["description"]
+        self.roles_ids: FrozenSet[int] = (
             frozenset(map(int, roles_ids)) if (roles_ids := data.get("role_ids")) else frozenset()
         )
-        self.channels_ids = (
+        self.channels_ids: FrozenSet[int] = (
             frozenset(map(int, channels_ids))
             if (channels_ids := data.get("channel_ids"))
             else frozenset()
         )
 
+        self.emoji: Optional[Union[Emoji, PartialEmoji]]
         if emoji_data := data.get("emoji"):
             self.emoji = guild._state.get_reaction_emoji(emoji_data)
         else:
