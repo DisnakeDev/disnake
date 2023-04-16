a3e00ece5fa1f1231ab219f5eb233c6a46cb4000
shiftinv <me@shiftinv.cc>
2023-04-16 19:09:28 +0200

    chore: soft-deprecate mypy plugin for now

--- disnake/ext/mypy_plugin/__init__.py
+++ disnake/ext/mypy_plugin/__init__.py
@@ -1,28 +1,13 @@
 # SPDX-License-Identifier: MIT
 
 import typing as t
-import warnings
 
 from mypy.plugin import Plugin
 
 
+# FIXME: properly deprecate this in the future
 class DisnakePlugin(Plugin):
-    """Deprecated mypy plugin."""
-
-    def __init__(self, *args: t.Any, **kwargs: t.Any) -> None:
-        super().__init__(*args, **kwargs)
-
-        # show deprecation warning when run
-        old_filters = warnings.filters[:]
-        try:
-            warnings.simplefilter("always", DeprecationWarning)
-            warnings.warn(
-                "disnake.ext.mypy_plugin is deprecated and a no-op",
-                category=DeprecationWarning,
-                stacklevel=2,
-            )
-        finally:
-            warnings.filters[:] = old_filters  # type: ignore
+    """Custom mypy plugin; no-op as of version 2.9."""
 
 
 def plugin(version: str) -> t.Type[Plugin]:
