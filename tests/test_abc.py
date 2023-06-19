47553e3fcfda5f3e188d21e9f8e3eaca27c5ae75
shiftinv <me@shiftinv.cc>
2023-06-19 13:59:13 +0200

    chore: move test method

--- tests/test_abc.py
+++ tests/test_abc.py
@@ -155,13 +155,13 @@ class TestGuildChannelEdit:
 
 
 class TestUserProtocol:
-    def handle_abc_user(self, user: disnake.abc.User) -> None:
-        ...
-
     def _test_typing_assignable(self) -> None:
+        def handle_abc_user(user: disnake.abc.User) -> None:
+            ...
+
         # All of these should match the abc.User protocol and thus type-check correctly
         # (they could just inherit from the protocol to ensure correct implementation,
         # but we really only want structural (i.e. implicit) subtyping)
-        self.handle_abc_user(cast(disnake.User, ...))
-        self.handle_abc_user(cast(disnake.ClientUser, ...))
-        self.handle_abc_user(cast(disnake.Member, ...))
+        handle_abc_user(cast(disnake.User, ...))
+        handle_abc_user(cast(disnake.ClientUser, ...))
+        handle_abc_user(cast(disnake.Member, ...))
