757bf1adb824f1b4ecc21e49026451fbde2eafc8
Snipy7374 <100313469+Snipy7374@users.noreply.github.com>
2023-06-23 23:36:39 +0200

    revert i'm crying

--- disnake/member.py
+++ disnake/member.py
@@ -698,6 +698,7 @@ class Member(disnake.abc.Messageable, _UserTag):
         """Optional[:class:`datetime.datetime`]: Returns the datetime when the timeout expires.
 
         If the member is not timed out or the timeout has already expired, returns ``None``.
+
         .. versionadded:: 2.3
         """
         if self._communication_disabled_until is None:
