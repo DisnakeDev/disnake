ed59df9e90c6dc634cd25e2e90e1af8b812ef475
shiftinv <me@shiftinv.cc>
2023-07-02 13:17:43 +0200

    feat: add sound create/edit/delete

--- disnake/soundboard.py
+++ disnake/soundboard.py
@@ -3,16 +3,16 @@
 from __future__ import annotations
 
 import datetime
-from typing import TYPE_CHECKING, Optional, Union
+from typing import TYPE_CHECKING, Any, Dict, Optional, Union
 
 from .asset import Asset, AssetMixin
 from .mixins import Hashable
-from .utils import _get_as_snowflake, snowflake_time
+from .partial_emoji import PartialEmoji
+from .utils import MISSING, _get_as_snowflake, snowflake_time
 
 if TYPE_CHECKING:
     from .emoji import Emoji
     from .guild import Guild
-    from .partial_emoji import PartialEmoji
     from .state import ConnectionState
     from .types.soundboard import (
         PartialSoundboardSound as PartialSoundboardSoundPayload,
@@ -91,10 +91,17 @@ class SoundboardSound(PartialSoundboardSound):
 
     _state: ConnectionState
 
-    def __init__(self, *, data: SoundboardSoundPayload, state: ConnectionState) -> None:
+    def __init__(
+        self,
+        *,
+        data: SoundboardSoundPayload,
+        state: ConnectionState,
+        # `guild_id` isn't sent over REST, so we manually keep track of it
+        guild_id: Optional[int],
+    ) -> None:
         super().__init__(data=data, state=state)
 
-        self.guild_id: Optional[int] = _get_as_snowflake(data, "guild_id")
+        self.guild_id: Optional[int] = guild_id or _get_as_snowflake(data, "guild_id")
         self.user: Optional[User] = (
             state.store_user(user_data) if (user_data := data.get("user")) is not None else None
         )
@@ -123,3 +130,42 @@ class SoundboardSound(PartialSoundboardSound):
         Could be ``None`` if the bot is not in the guild.
         """
         return self._state._get_guild(self.guild_id)
+
+    async def edit(
+        self,
+        *,
+        name: str = MISSING,
+        volume: float = MISSING,
+        emoji: Optional[Union[str, Emoji, PartialEmoji]] = MISSING,
+        reason: Optional[str] = None,
+    ) -> SoundboardSound:
+        """TODO"""
+        payload: Dict[str, Any] = {}
+
+        # FIXME: workaround for API issue, which clears volume + emoji if not provided
+        if volume is MISSING:
+            volume = self.volume
+        if emoji is MISSING:
+            emoji = self.emoji
+
+        if name is not MISSING:
+            payload["name"] = name
+        if volume is not MISSING:
+            payload["volume"] = volume
+        if emoji is not MISSING:
+            emoji_name, emoji_id = PartialEmoji._emoji_to_name_id(emoji)
+            payload["emoji_name"] = emoji_name
+            payload["emoji_id"] = emoji_id
+
+        if not self.guild_id:
+            raise RuntimeError  # default sound  # TODO: improve this, split into two classes or raise proper error here
+        data = await self._state.http.edit_guild_soundboard_sound(
+            self.guild_id, self.id, reason=reason, **payload
+        )
+        return SoundboardSound(data=data, state=self._state, guild_id=self.guild_id)
+
+    async def delete(self, *, reason: Optional[str] = None) -> None:
+        """TODO"""
+        if not self.guild_id:
+            raise RuntimeError  # default sound  # TODO: see above
+        await self._state.http.delete_guild_soundboard_sound(self.guild_id, self.id, reason=reason)
