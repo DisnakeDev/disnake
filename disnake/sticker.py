35915569531552d93145dd7e4ceff202f0c1a70f
shiftinv <8530778+shiftinv@users.noreply.github.com>
2023-10-26 15:53:04 +0200

    docs: make `Supported Operations` container collapsible (#1126)

--- disnake/sticker.py
+++ disnake/sticker.py
@@ -44,7 +44,7 @@ class StickerPack(Hashable):
     .. versionchanged:: 2.8
         :attr:`cover_sticker_id`, :attr:`cover_sticker` and :attr:`banner` are now optional.
 
-    .. container:: operations
+    .. collapse:: operations
 
         .. describe:: str(x)
 
@@ -163,7 +163,7 @@ class StickerItem(_StickerTag):
 
     .. versionadded:: 2.0
 
-    .. container:: operations
+    .. collapse:: operations
 
         .. describe:: str(x)
 
@@ -226,7 +226,7 @@ class Sticker(_StickerTag):
 
     .. versionadded:: 1.6
 
-    .. container:: operations
+    .. collapse:: operations
 
         .. describe:: str(x)
 
@@ -283,7 +283,7 @@ class StandardSticker(Sticker):
 
     .. versionadded:: 2.0
 
-    .. container:: operations
+    .. collapse:: operations
 
         .. describe:: str(x)
 
@@ -362,7 +362,7 @@ class GuildSticker(Sticker):
 
     .. versionadded:: 2.0
 
-    .. container:: operations
+    .. collapse:: operations
 
         .. describe:: str(x)
 
