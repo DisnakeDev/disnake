9157a0a0cb7d385572d177dabed512eaf19a39d8
shiftinv <8530778+shiftinv@users.noreply.github.com>
2023-11-22 14:46:55 +0100

    fix(docs): fix broken references to core
    Signed-off-by: shiftinv <8530778+shiftinv@users.noreply.github.com>

--- disnake/ext/commands/slash_core.py
+++ disnake/ext/commands/slash_core.py
@@ -332,7 +332,7 @@ class SubCommand(InvokableApplicationCommand):
 
     @property
     def description(self) -> str:
-        """:class:`str`: The slash sub command's description. Shorthand for :attr:`self.body.description <Option.description>`."""
+        """:class:`str`: The slash sub command's description. Shorthand for :attr:`self.body.description <.Option.description>`."""
         return self.body.description
 
     @property
@@ -510,12 +510,12 @@ class InvokableSlashCommand(InvokableApplicationCommand):
 
     @property
     def description(self) -> str:
-        """:class:`str`: The slash command's description. Shorthand for :attr:`self.body.description <SlashCommand.description>`."""
+        """:class:`str`: The slash command's description. Shorthand for :attr:`self.body.description <.SlashCommand.description>`."""
         return self.body.description
 
     @property
     def options(self) -> List[Option]:
-        """List[:class:`.Option`]: The list of options the slash command has. Shorthand for :attr:`self.body.options <SlashCommand.options>`."""
+        """List[:class:`.Option`]: The list of options the slash command has. Shorthand for :attr:`self.body.options <.SlashCommand.options>`."""
         return self.body.options
 
     def sub_command(
