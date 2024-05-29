6db7054c07c8ff8fc2c047c0478c488d8587de82
Victor <67214928+Victorsitou@users.noreply.github.com>
2024-05-29 16:13:21 -0400

    feat: implement avatar decorations (#889)

--- disnake/asset.py
+++ disnake/asset.py
@@ -317,6 +317,16 @@ class Asset(AssetMixin):
             animated=False,
         )
 
+    @classmethod
+    def _from_avatar_decoration(cls, state: AnyState, avatar_decoration_asset: str) -> Self:
+        animated = avatar_decoration_asset.startswith("a_")
+        return cls(
+            state,
+            url=f"{cls.BASE}/avatar-decoration-presets/{avatar_decoration_asset}.png?size=1024",
+            key=avatar_decoration_asset,
+            animated=animated,
+        )
+
     def __str__(self) -> str:
         return self._url
 
