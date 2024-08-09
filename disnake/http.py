6cbe91be986add5423119f2db98aa5f2f68b5b20
Snipy7374 <100313469+Snipy7374@users.noreply.github.com>
2024-08-09 16:07:41 +0200

    remove unimplemented endpoint

--- disnake/http.py
+++ disnake/http.py
@@ -935,10 +935,6 @@ class HTTPClient:
         }
         return self.request(r, json=payload, reason=reason)
 
-    def get_my_voice_state(self, guild_id: Snowflake) -> Response[voice.GuildVoiceState]:
-        r = Route("GET", "/guilds/{guild_id}/voice-states/@me", guild_id=guild_id)
-        return self.request(r)
-
     def get_voice_state(
         self, guild_id: Snowflake, user_id: Snowflake
     ) -> Response[voice.GuildVoiceState]:
