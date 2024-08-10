4cfd80502c3b5a69a7103b252e8e85cd8af8d129
Snipy7374 <100313469+Snipy7374@users.noreply.github.com>
2024-08-10 18:17:14 +0200

    implement new sticker pack endpoint

--- disnake/client.py
+++ disnake/client.py
@@ -2512,6 +2512,28 @@ class Client:
         cls, _ = _sticker_factory(data["type"])  # type: ignore
         return cls(state=self._connection, data=data)  # type: ignore
 
+    async def fetch_sticker_pack(self, pack_id: int, /) -> StickerPack:
+        """|coro|
+
+        Retrieves a :class:`.StickerPack` with the given ID.
+
+        .. versionadded:: 2.10
+
+        Raises
+        ------
+        HTTPException
+            Retrieving the sticker pack failed.
+        NotFound
+            Invalid sticker pack ID.
+
+        Returns
+        -------
+        :class:`.StickerPack`
+            The sticker pack you requested.
+        """
+        data = await self.http.get_sticker_pack(pack_id)
+        return StickerPack(state=self._connection, data=data)
+
     async def fetch_sticker_packs(self) -> List[StickerPack]:
         """|coro|
 
