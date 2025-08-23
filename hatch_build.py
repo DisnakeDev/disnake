9c149b92436ae525b513127f8b97cc02552b8fbe
onerandomusername <me@arielle.codes>
2025-08-23 16:53:21 -0400

    fix: remove discord shim

--- hatch_build.py
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: MIT
-
-from hatchling.metadata.plugin.interface import MetadataHookInterface
-
-
-class CustomMetadataHook(MetadataHookInterface):
-    def update(self, metadata: dict) -> None:
-        metadata["optional-dependencies"]["discord"] = ["discord-disnake"]
