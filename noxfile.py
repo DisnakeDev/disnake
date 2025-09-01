719e5d0e018d4e057f34221fda93f99673d6375c
onerandomusername <me@arielle.codes>
2025-09-01 17:41:07 -0400

    fix: draw python test versions from classifers

--- noxfile.py
+++ noxfile.py
@@ -249,7 +249,7 @@ def pyright(session: nox.Session) -> None:
         pass
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"])
+@nox.session(python=nox.project.python_versions(PYPROJECT))
 @nox.parametrize(
     "extras",
     [
