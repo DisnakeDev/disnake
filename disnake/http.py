44288fc0f8dc2eaef5fb4f6fdafd352799aefb2b
onerandomusername <me@arielle.codes>
2025-09-04 16:07:28 -0400

    try setting aiohttp elsewhere

--- disnake/http.py
+++ disnake/http.py
@@ -257,14 +257,16 @@ class HTTPClient:
             )
 
     async def ws_connect(self, url: str, *, compress: int = 0) -> aiohttp.ClientWebSocketResponse:
+        if hasattr(aiohttp, "ClientWSTimeout"):
+            timeout = aiohttp.ClientWSTimeout(ws_close=30.0)  # pyright: ignore
+        else:
+            timeout = 30.0
         return await self.__session.ws_connect(
             url,
             proxy_auth=self.proxy_auth,
             proxy=self.proxy,
             max_msg_size=0,
-            timeout=aiohttp.ClientWSTimeout(ws_close=30.0)  # type: ignore
-            if hasattr(aiohttp, "ClientWSTimeout")
-            else 30.0,
+            timeout=timeout,
             autoclose=False,
             headers={
                 "User-Agent": self.user_agent,
