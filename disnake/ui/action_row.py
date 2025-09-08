2115bb2ef12d21ead6f2d6aba2fb22f26fcba9df
shiftinv <me@shiftinv.cc>
2025-09-08 15:21:30 +0200

    docs: reapply documentation fixes from previous commit

--- disnake/ui/action_row.py
+++ disnake/ui/action_row.py
@@ -701,7 +701,7 @@ class ActionRow(UIComponent, Generic[ActionRowChildT]):
         )
         return self
 
-    @deprecated()
+    @deprecated('Label("<text>", TextInput(...))')
     def add_text_input(
         self: TextInputCompatibleActionRowT,
         *,
@@ -726,8 +726,8 @@ class ActionRow(UIComponent, Generic[ActionRowChildT]):
         .. versionadded:: 2.4
 
         .. deprecated:: 2.11
-            Use of action rows in modals is deprecated, use ``Label("...", TextInput(...))``
-            directly instead.
+            Use of action rows in modals is deprecated, use
+            ``Label("<text>", TextInput(...))`` directly instead.
 
         Parameters
         ----------
