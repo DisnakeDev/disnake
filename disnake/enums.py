1e005216b81bf3070c005ed676d71e7b03d5f96e
arielle <me@arielle.codes>
2025-09-10 12:37:55 -0400

    feat: add `emoji_added` message type and system channel flag (#1370)

--- disnake/enums.py
+++ disnake/enums.py
@@ -430,6 +430,11 @@ class MessageType(Enum):
 
     .. versionadded:: 2.10
     """
+    emoji_added = 63
+    """The system message denoting that an emoji was added to the server.
+
+    .. versionadded: 2.11
+    """
 
 
 class PartyType(Enum):
