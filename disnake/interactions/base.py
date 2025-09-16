3d6017f8a36c57bfddae5842cb5e0ff31a4020ef
onerandomusername <me@arielle.codes>
2025-09-15 23:32:18 -0400

    lint: enable RET rules

--- disnake/interactions/base.py
+++ disnake/interactions/base.py
@@ -1994,6 +1994,7 @@ class InteractionMessage(Message):
             asyncio.create_task(inner_call())
         else:
             await self._state._interaction.delete_original_response()
+        return None
 
 
 class InteractionDataResolved(Dict[str, Any]):
