18b492dc1860cb8f5bc224667aa67d53c9a851d6
vi <me@shiftinv.cc>
2026-05-25 11:41:05 +0200

    fix(test): no need for these to be async

--- tests/ext/commands/test_converter.py
+++ tests/ext/commands/test_converter.py
@@ -9,13 +9,11 @@ from disnake.ext import commands
 
 class TestGreedy:
     @pytest.mark.parametrize("param", [int, float | bool])
-    @pytest.mark.asyncio
-    async def test_valid(self, param) -> None:
+    def test_valid(self, param) -> None:
         # should not raise
         commands.Greedy[param]
 
     @pytest.mark.parametrize("param", [None, int | None, Union[None, bool]])  # noqa: UP007
-    @pytest.mark.asyncio
-    async def test_invalid(self, param) -> None:
+    def test_invalid(self, param) -> None:
         with pytest.raises(TypeError):
             commands.Greedy[param]
