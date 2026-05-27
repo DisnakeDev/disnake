cf7cc07ac36c7314c3887c86d32ec42c0453f879
vi <me@shiftinv.cc>
2026-05-27 18:30:47 +0200

    docs: implement new custom search scorer
    For the most part, this scores things identically to Sphinx's builtin
    scorer, with two notable differences:
    
    - object name substring matches (outside the last dotted part) are ranked
      higher than fulltext matches, since the latter are fairly useless in most cases
    - for full name matches (i.e. either exact match or at least exact match of last dotted part),
      results are reordered to show shorter (by dotted parts) results first, in an attempt to show
      e.g. `disnake.Client` before `disnake.ApplicationCommandInteraction.client`, since those would
      otherwise just be ordered alphabetically, while someone searching for "Client" would most likely
      be more interested in the former than the latter

--- docs/_static/scorer.js
+++ docs/_static/scorer.js
@@ -2,17 +2,37 @@
 
 "use strict";
 
+let queryParts = undefined;
+
+function __getQuery() {
+    const params = new URLSearchParams(window.location.search);
+    const query = params.get('q');
+    queryParts = query ? query.split(/\s+/g).map((s) => s.toLowerCase()) : null;
+}
 
 Scorer = {
     // Implement the following function to further tweak the score for each result
     // The function takes a result array [docname, title, anchor, descr, score, filename]
     // and returns the new score.
-    /*
-    score: result => {
-      const [docname, title, anchor, descr, score, filename, kind] = result
-      return score
+    score: (result) => {
+        const [docname, title, anchor, descr, score, filename, kind] = result;
+
+        if (queryParts === undefined) __getQuery();
+        if (queryParts === null) return score;
+
+        // For object name matches where the last dotted part exactly matches the query,
+        // prioritize shorter (by parts) object names over longer ones, given otherwise same score.
+        // (this then results in e.g. `disnake.Intents` appearing before `disnake.Client.intents` or
+        // `disnake.ext.commands.Bot.intents`, which would all have the same score)
+        if (kind === "object") {
+            const parts = title.split(".");
+            if (queryParts.includes(parts.slice(-1)[0].toLowerCase())) {
+                return score + (1 - parts.length * 0.05);
+            }
+        }
+
+        return score;
     },
-    */
 
     // query matches the full name of an object
     objNameMatch: 11,
@@ -21,7 +41,8 @@ Scorer = {
     // Additive scores depending on the priority of the object
     objPrio: {
         0: 15, // used to be importantResults
-        1: 5,  // used to be objectResults
+        // changed 5->7 to generally rank object name matches higher than page term matches
+        1: 7,  // used to be objectResults
         2: -5, // used to be unimportantResults
     },
     // Used when the priority is not in the mapping.
