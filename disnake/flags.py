e23df8a62b4a30ad82f8128e4d1703f0066a9ba2
vi <me@shiftinv.cc>
2026-08-15 10:50:23 +0200

    docs: change privileged intent requirement to 10000 users

--- disnake/flags.py
+++ disnake/flags.py
@@ -1190,7 +1190,7 @@ class Intents(BaseFlags):
         .. note::
 
             Currently, this requires opting in explicitly via the developer portal as well.
-            Bots in over 100 guilds will need to apply to Discord for verification.
+            Bots with more than 10000 users will need to apply to Discord for verification.
         """
         return 1 << 1
 
@@ -1349,7 +1349,7 @@ class Intents(BaseFlags):
         .. note::
 
             Currently, this requires opting in explicitly via the developer portal as well.
-            Bots in over 100 guilds will need to apply to Discord for verification.
+            Bots with more than 10000 users will need to apply to Discord for verification.
         """
         return 1 << 8
 
@@ -1469,7 +1469,7 @@ class Intents(BaseFlags):
         .. note::
 
             Currently, this requires opting in explicitly via the developer portal as well.
-            Bots in over 100 guilds will need to apply to Discord for verification.
+            Bots with more than 10000 users will need to apply to Discord for verification.
         """
         return 1 << 15
 
